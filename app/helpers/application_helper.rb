a6de19af11a9c441cf0601b3ee70e7eedf24bb4b
Pete Krautscheid <krautscheid@mitre.org>
2013-10-08 10:15:57 -0400

    Minimize JSON of measure when bootstrapping

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def measure_json(measure, population, population_count)
     # For use in client code we set the ID to be of the form 0004a
     sub_id = if population_count > 1 then ('a'..'z').to_a[population] else '' end
-    measure.as_json.merge(id: "#{measure.measure_id}#{sub_id}").to_json
+    measure.as_json(except: [:map_fns]).merge(id: "#{measure.measure_id}#{sub_id}").to_json
   end
 
   def flash_class(level)
