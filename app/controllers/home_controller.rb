9146a17bd9bdbd18c2617ca9ca1da667014011bf
Pete Krautscheid <krautscheid@mitre.org>
2013-10-16 14:27:01 -0400

    In development only, set up all measures for user who has none

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
 
   def index
     @measures = current_user.measures.asc(:measure_id)
+    if Rails.env.development?
+      # FIXME: At least for now, for development, let us see all the measures
+      @measures = Measure.asc(:measure_id) if @measures.blank?
+    end
     @patients = Record.asc(:last, :first)
   end
 
