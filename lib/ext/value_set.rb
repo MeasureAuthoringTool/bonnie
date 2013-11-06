b23e5f55f1c5cab1d61ba3abced156fb6596b095
Pete Krautscheid <krautscheid@mitre.org>
2013-11-06 14:22:54 -0500

    Don't alter ValueSet serialization for client side, it affects measure JS

--- lib/ext/value_set.rb
+++ lib/ext/value_set.rb
@@ -1,9 +1,10 @@
 module HealthDataStandards
   module SVS
     class ValueSet
-      def serializable_hash(options=nil)
-        super({ only: [:oid, :display_name] }.merge(options || {}))
-      end
+      # FIXME: We'd like to minimize our client side footprint, but this interferes with JS generation
+      # def serializable_hash(options=nil)
+      #   super({ only: [:oid, :display_name] }.merge(options || {}))
+      # end
     end
   end
 end
