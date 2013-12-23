6e9ef1191f62403489b8abc6456929e975a70c3f
Andre Quina <aquina@mitre.org>
2013-12-23 11:59:35 -0500

    cleaned up patient builder code and moved patient clearing code together with section entry generation code

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,7 +2,6 @@ class PatientsController < ApplicationController
 	before_filter :authenticate_user!
 	# before_filter :validate_authorization!
 
-  JAN_ONE_THREE_THOUSAND=32503698000000
   RACE_NAME_MAP={'1002-5' => 'American Indian or Alaska Native','2028-9' => 'Asian','2054-5' => 'Black or African American','2076-8' => 'Native Hawaiian or Other Pacific Islander','2106-3' => 'White','2131-1' => 'Other'}
   ETHNICITY_NAME_MAP={'2186-5'=>'Not Hispanic or Latino', '2135-2'=>'Hispanic Or Latino'}
 
@@ -88,18 +87,13 @@ class PatientsController < ApplicationController
     redirect_to patients_path
   end
 
-
-
 private 
 
 
   def update_patient(patient)
 
-    patient['measure_id'] ||= params['measure_id']
     patient['measure_ids'] ||= []
     patient['measure_ids'] << params['measure_id'] unless patient['measure_ids'].include? params['measure_id']
-
-    # patient['birthdate'] = Time.parse(params['birthdate']).to_i
 
     ['first', 'last', 'gender', 'expired', 'birthdate', 'description', 'description_category'].each {|param| patient[param] = params[param]}
 
@@ -113,31 +107,20 @@ private
     params['source_data_criteria'].each { |c| c[:value] ||= [] }
 
     patient['source_data_criteria'] = params['source_data_criteria'] + [measure_period]
-    patient['measure_period_start'] = measure_period['start_date']
-    patient['measure_period_end'] = measure_period['end_date']
-
-    insurance_types = {
-      'MA' => 'Medicare',
-      'MC' => 'Medicaid',
-      'OT' => 'Other'
-    }
-
-    insurance_codes = {
-      'MA' => '1',
-      'MC' => '2',
-      'OT' => '349'
-    }
+    # TODO: we should probably get the target measure period on the patient builder view
+    # patient['measure_period_start'] = measure_period['start_date']
+    # patient['measure_period_end'] = measure_period['end_date']
 
     insurance_provider = InsuranceProvider.new
     insurance_provider.type = params['payer']
     insurance_provider.member_id = '1234567890'
-    insurance_provider.name = insurance_types[params['payer']]
+    insurance_provider.name = Measures::PatientBuilder::INSURANCE_TYPES[params['payer']]
     insurance_provider.financial_responsibility_type = {'code' => 'SELF', 'codeSystem' => 'HL7 Relationship Code'}
     insurance_provider.start_time = Time.new(2008,1,1).to_i
     insurance_provider.payer = Organization.new
     insurance_provider.payer.name = insurance_provider.name
-    insurance_provider.codes["SOP"] = [insurance_codes[params['payer']]]
-    patient.insurance_providers = [insurance_provider]
+    insurance_provider.codes["SOP"] = [Measures::PatientBuilder::INSURANCE_CODES[params['payer']]]
+    patient.insurance_providers.clear << insurance_provider
 
     patient.expected_values = params['expected_values']
 
