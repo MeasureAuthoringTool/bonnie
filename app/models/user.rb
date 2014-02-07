2d6b4a5d15a554f37bbb213df23e18f9e46995db
Fernandes <smoothclip@gmail.com>
2014-02-07 11:37:09 -0500

    Add first_name, last_name, and telephone as accessible for account page

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User
   end
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :telephone
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
