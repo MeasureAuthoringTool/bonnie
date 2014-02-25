830dd47b48faeeb0824f2b0c5bbcbe96a332d74b
Pete Krautscheid <krautscheid@mitre.org>
2014-02-25 12:12:54 -0500

    Fixed issue caused by removing un-needed template file and added test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @measures = Measure.by_user(current_user).only(:id) # Only using measure for JS URL generation, only need ID
     @patients = Record.by_user(current_user)
+    render nothing: true, layout: true
   end
 
 end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+  setup do
+    dump_database
+    collection_fixtures("draft_measures", "users")
+    @user = User.by_email('bonnie@example.com').first
+    associate_user_with_measures(@user,Measure.all)
+    @measure = Measure.where({"cms_id" => "CMS138v2"}).first
+    sign_in @user
+  end
+  test "index" do
+    get :index
+    assert_response :success
+  end
 end
