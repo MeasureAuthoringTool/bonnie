5547beaa4a0ca1196e047c8d26acc025ba15f883
Pete Krautscheid <krautscheid@mitre.org>
2014-03-27 14:41:53 -0400

    Further email configuration for production deployments

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Bonnie::Application.initialize!
-
 # Determine our hostname; first look for it specified in a config file, else query OS
 HOSTNAME = (File.read(Rails.root.join('config', 'hostname')).chomp rescue nil) || `hostname`.chomp
+
+# Initialize the rails application
+Bonnie::Application.initialize!
