3c843e1b8f3d657196e867b759632919533b2d7c
krautscheid Krautscheid <krautscheid@mitre.org>
2014-04-03 14:55:42 -0400

    Configure test host URL to address failing tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,6 +29,7 @@ Bonnie::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  Rails.application.routes.default_url_options[:host] = 'localhost'
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
