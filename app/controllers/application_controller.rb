84de58b92fb7d89fafa4ba353d3912f8f666913b
Dan Potter <dpotter@mitre.org>
2014-05-19 13:27:19 -0400

    Back out email change that shouldn't have been merged with the SASS branch, rearrange where bootstrap code is imported

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
-  before_filter :log_additional_data
   layout :layout_by_resource
 
   def layout_by_resource
@@ -12,10 +11,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  protected
-    def log_additional_data
-      request.env["exception_notifier.exception_data"] = {
-        :current_user => current_user
-      }
-    end
 end
