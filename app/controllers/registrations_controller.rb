526385dd8d715b83ee8e48dd0d5adb900d2e358e
Dan Potter <dpotter@mitre.org>
2014-06-10 14:57:37 -0400

    Make the delete process require a confirmation on a separate page to make it much more difficult to accidentally delete your account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,8 +15,36 @@ class RegistrationsController < Devise::RegistrationsController
     "#{(respond_to?(:root_path) ? root_path : "/")}users/sign_in"
   end
 
+  # Store the original destroy so we can use it later
+  alias :destroy_user_account :destroy
+
+  def destroy
+    # Override the superclass destroy method to prevent the user from instantly
+    # deleting their account just by visiting this page.
+    # TODO (maybe): Require the user to enter their password to delete their
+    # account?
+    if params[:reset_token] == 'true' or (session.has_key?(:account_delete_token_expires) and session[:account_delete_token_expires] < Time.now)
+      # Reset or expire the token
+      session.delete(:account_delete_token)
+      session.delete(:account_delete_token_expires)
+    end
+    if session.has_key?(:account_delete_token)
+      if params[:delete_token] == session[:account_delete_token]
+        # Invoke the parent method
+        destroy_user_account
+      else
+        @delete_denied = true
+      end
+    else
+      # If we have no token, add it to the session
+      session[:account_delete_token] = SecureRandom.urlsafe_base64(nil, false)
+      # And add a max time
+      session[:account_delete_token_expires] = Time.now + (60 * 60)
+    end
+  end
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) << :crosswalk_enabled
   end
