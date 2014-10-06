26124983ca9f12a3d20239637acac9c99e7045e2
Pete K <krautscheid@mitre.org>
2014-10-06 14:15:16 -0400

    Separate out a server specific config file

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,9 @@ require File.expand_path('../application', __FILE__)
 # Load config here, not in bonnie initializer, so config is available during initialization
 APP_CONFIG = YAML.load_file(Rails.root.join('config', 'bonnie.yml'))[Rails.env]
 
+# Also load in a server specific config file, if present, for things like mail server config
+APP_CONFIG.merge! YAML.load_file(Rails.root.join('config', 'server.yml')) if File.exists? Rails.root.join('config', 'server.yml')
+
 # Determine our hostname if not specified in config file
 APP_CONFIG['hostname'] ||= `hostname`.chomp
 
