e02160b75141479d3819e0a805c4fb25c152ebb6
Pete K <krautscheid@mitre.org>
2015-03-26 23:30:10 -0400

    Remove logging from debug view and only show measure patients by default

--- lib/bonnie_measure_javascript.rb
+++ lib/bonnie_measure_javascript.rb
@@ -7,10 +7,13 @@ module BonnieMeasureJavascript
   # each population, and attaches the measure to the bonnie router object; this is used both on the front end
   # as well as within our tests
 
-  # The options can be set to those expected by the measure JS code: clear_db_cache and cache_result_in_db
+  # The options can be set to not display the rationale, as well as those expected by the measure JS code:
+  # clear_db_cache and cache_result_in_db
 
   def self.generate_for_population(measure, population_index, options = {})
-    population_javascript = measure.map_fn(population_index, options)
+    options.reverse_merge! rationale: true # Generate rationale by default
+    rationale = !!options[:rationale]
+    population_javascript = measure.map_fn(population_index, options.except(:rationale))
     JAVASCRIPT_TEMPLATE.result binding
   end
 
