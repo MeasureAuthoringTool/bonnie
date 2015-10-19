8a0f0596aa86411ba97a9f83d14243f17c25b181
alexanderelliott121 <aelliott@mm191023-pc.mitre.org>
2015-10-19 10:12:40 -0400

    Add versioned_oid field to value_set wrapper.

--- lib/ext/value_set.rb
+++ lib/ext/value_set.rb
@@ -1,6 +1,7 @@
 module HealthDataStandards
   module SVS
     class ValueSet
+      field :versioned_oid, type: String
       # FIXME: We'd like to minimize our client side footprint, but this interferes with JS generation
       # def serializable_hash(options=nil)
       #   super({ only: [:oid, :display_name] }.merge(options || {}))
