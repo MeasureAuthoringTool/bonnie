16240b6158ed1631aaa4944036686a5c2c5512d7
Chris Hossenlopp <hossenlopp@mitre.org>
2016-02-08 09:43:35 -0500

    Initial implementation of create API call.

--- app/controllers/api_v1/measures_controller.rb
+++ app/controllers/api_v1/measures_controller.rb
@@ -1,12 +1,88 @@
 class ApiV1::MeasuresController < ApplicationController
 
+  class IntegerValidator < Apipie::Validator::BaseValidator
+
+  def initialize(param_description, argument)
+    super(param_description)
+    @type = argument
+  end
+
+  def validate(value)
+    return false if value.nil?
+    !!(value.to_s =~ /^[-+]?[0-9]+$/)
+  end
+
+  def self.build(param_description, argument, options, block)
+    if argument == Integer || argument == Fixnum
+      self.new(param_description, argument)
+    end
+  end
+
+  def description
+    "Must be #{@type}."
+  end
+end
+
+  class MeasureFileValidator < Apipie::Validator::BaseValidator
+    def initialize(param_description, argument)
+      super(param_description)
+      @type = argument
+    end
+      
+    def validate(value)
+      if !(value.is_a?(Rack::Test::UploadedFile) || value.is_a?(ActionDispatch::Http::UploadedFile))
+        return false
+      end
+
+      # Understand which sort of measure_file we are retrieving
+      extension = File.extname(value.original_filename).downcase if value
+      if extension == '.zip'
+        return Measures::MATLoader.mat_export?(value)
+      elsif extension == '.xml'
+        return true
+      else
+        return false
+      end
+    end
+
+    def self.build(param_description, argument, options, block)
+      self.new(param_description, argument) if argument == File
+    end
+
+    def description
+      'Must be a valid MAT Export or HQMF File.'
+    end
+  end
+  
+  class DateValidator < Apipie::Validator::BaseValidator
+    def initialize(param_description, argument)
+      super(param_description)
+      @type = argument
+    end
+      
+    def validate(value)
+      Date.strptime(value,'%m/%d/%Y') rescue return false
+      return true
+    end
+
+    def self.build(param_description, argument, options, block)
+      self.new(param_description, argument) if argument == Date
+    end
+
+    def description
+      'Must be a date in the form mm/dd/yyyy.'
+    end
+  end
+
   MEASURE_WHITELIST = ["id", "cms_id", "complexity", "continuous_variable", "created_at", "description", "episode_of_care", "hqmf_id", "hqmf_set_id", "hqmf_version_number", "title", "type", "updated_at"]
   PATIENT_WHITELIST = ["_id", "birthdate", "created_at", "deathdate", "description", "ethnicity", "expected_values", "expired", "first", "gender", "insurance_providers", "last", "notes", "race", "updated_at"]
   INSURANCE_WHITELIST = ["member_id","payer"]
   POPULATION_TYPES = ['population_index','STRAT','IPP','DENOM','NUMER','DENEXCEP','DENEX','MSRPOPL','OBSERV','MSRPOPLEX']
 
   respond_to :json, :html
+  rescue_from Apipie::ParamMissing, :with => :error_parameter_missing
   rescue_from ActionController::ParameterMissing, :with => :error_parameter_missing
+  rescue_from Apipie::ParamInvalid, :with => :error_parameter_invalid
 
   resource_description do
     formats [:json]
@@ -17,6 +93,18 @@ class ApiV1::MeasuresController < ApplicationController
   def_param_group :measure do
     param :id, String, :required => true, :desc => 'The HQMF Set ID of the Measure.'
     error :code => 404, :desc => 'Not Found'
+  end
+  
+  def_param_group :measure_upload do
+    param :measure_file, File, :required => true, :desc => "The measure file."
+    param :measure_type, ["eh", "ep"], :required => true, :desc => "The type of the measure."
+    param :calculation_type, ["episode", "patient"], :required => true, :desc => "The type of calculation."
+    param :episode_of_care, Integer, :required => false, :desc => "The index of the episode of care. Defaults to 0. This means that the first specific occurence in the logic will be used for the episode of care calculation."
+    param :population_titles, Array, of: String, :required => false, :desc => "The titles of the populations. If this is not included, populations will assume default values. i.e. \"Population 1\", \"Population 2\", etc."
+    param :vsac_username, String, :required => false, :desc => "Username for VSAC. Required when uploading a HQMF .xml measure."
+    param :vsac_password, String, :required => false, :desc => "Password for VSAC. Required when uploading a HQMF .xml measure."
+    param :include_draft, Boolean, :required => false, :desc => "If VSAC should fetch draft value sets. Defaults to 'true' if not supplied."
+    param :vsac_date, Date, :required => false, :desc => "The lastest effective date for published value sets to retrieve. Required when include_draft is false."
   end
 
   api :GET, '/api_v1/measures', 'List of Measures'
@@ -149,47 +237,19 @@ class ApiV1::MeasuresController < ApplicationController
 
   api :POST, '/api_v1/measures', 'Create a New Measure'
   description 'Creating a new measure.'
+  formats ["multipart/form-data"]
+  error :code => 400, :desc => "Client sent bad parameters. Response contains explanation."
+  error :code => 409, :desc => "Measure with this HQMF Set ID already exists."
+  error :code => 500, :desc => "A server error occured."
+  param_group :measure_upload
   def create
-    # ensure required params exist
-    params.require(:measure_file)
-    params.require(:measure_type)
-    params.require(:calculation_type)
-    
-    # ensure other required params have valid values
-    if !['eh', 'ep'].include?(params[:measure_type])
-      render json: {status: "error", messages: "Invalid value: measure_type must be 'eh' or 'ep'."},
-             status: :bad_request
-      return
-    end
-    
-    if !['episode', 'patient'].include?(params[:calculation_type])
-      render json: {status: "error", messages: "Invalid value: calculation_type must be 'patient' or 'episode'."},
-             status: :bad_request
-      return
-    end
-    
-    # Ensure that measure_file is indeed a file
-    if !params[:measure_file].respond_to?('original_filename')
-      # return with an error and 400 status
-      render json: {status: "error", messages: "Invalid parameter: measure_file must be a file" },
-             status: :bad_request
-      return
-    end
-    measure_file = params[:measure_file]
-    
     # Understand which sort of measure_file we are retrieving
     extension = File.extname(params[:measure_file].original_filename).downcase if params[:measure_file]
     
     begin
-      # See if it is a MAT Export and try loading it.
+      # If it is a MAT export
       if extension == '.zip'
-        if !Measures::MATLoader.mat_export?(params[:measure_file])
-          render json: {status: "error", messages: "measure_file does not appear to be a MAT export." },
-                 status: :bad_request
-          return
-        else
-          measure = load_mat_export(params)
-        end
+        measure = load_mat_export(params)
         
       # If it is a measure file.
       elsif extension == '.xml'
@@ -198,20 +258,76 @@ class ApiV1::MeasuresController < ApplicationController
         params.require(:vsac_password)
         include_draft = params.fetch(:include_draft, true)
         params.require(:vsac_date) unless include_draft
+      end
+      
+      # Handle episode of care measurements.
+      if params['calculation_type'] == 'episode'
+        specific_occurrences = measure.source_data_criteria.values.select {|d| d['specific_occurrence']}
         
-        
-      else
-        render json: {status: "error", messages: "Incorrect measure_file format." },
+        # if there are no specific_occurrences then episode of care cannot be measured.
+        if specific_occurrences.empty?
+          measure.delete
+          render json: {status: "error", messages: "Episode of care calculation was specified. Episode of care measures require at lease one data element that is a specific occurrence.  Please add a specific occurrence data element to the measure logic."},
                status: :bad_request
+          return
+        else
+          eocIndex = params.fetch('episode_of_care', '0').to_i
+          if eocIndex >= specific_occurrences.length
+            render json: {status: "error", messages: "The episode_of_care index is out of bounds of the set of specific occurrences found in the mesasure."},
+                 status: :bad_request
+            return
+          end
+          measure.update_attributes({episode_ids: [specific_occurrences[eocIndex]["source_data_criteria"]]})
+        end
+      end
+      
+      # exclude patient birthdate and expired OIDs used by SimpleXML parser for AGE_AT handling and bad oid protection in missing VS check
+      missing_value_sets = (measure.as_hqmf_model.all_code_set_oids - measure.value_set_oids - ['2.16.840.1.113883.3.117.1.7.1.70', '2.16.840.1.113883.3.117.1.7.1.309'])
+      if missing_value_sets.length > 0
+        measure.delete
+        render json: {status: "Error", messages: "The measure you have tried to load is missing value sets. Try re-packaging and re-exporting the measure from the Measure Authoring Tool.  The following value sets are missing: [#{missing_value_sets.join(', ')}]"},
+             status: :bad_request
         return
       end
-
-    rescue Exception => e
-      render json: {status: "error", messages: e }
+      
+      # Handle population naming. Make default names if none or not enough were provided.
+      if measure.populations.size > 1
+        pop_titles = params.fetch('population_titles', [])
+        strat_index = 0
+        measure.populations.each.with_index do |population, index|
+          if (population[HQMF::PopulationCriteria::STRAT])
+            population['title'] = pop_titles.fetch(index, "Stratification #{strat_index + 1}")
+            strat_index += 1
+          elsif index < pop_titles.size
+            population['title'] = pop_titles.fetch(index)
+          end
+        end
+      end
+      
+    rescue Measures::ValueSetException
+      render json: {status: "error", messages: "The measure value sets could not be found. Please re-package the measure in the MAT and make sure &quot;VSAC Value Sets&quot; are included in the package, then re-export the MAT Measure bundle." },
+           status: :bad_request
       return
+    rescue Exception => e
+      raise e
+      #render json: {status: "error", messages: e }, status: :error
+      #return
     end
     
-    render json: {status: "success", url: "/api_v1/measures/#{measure.hqmf_set_id}"}, status: :ok
+    # Make sure we didn't create a duplicate measure
+    existing = Measure.by_user(current_user).where(hqmf_set_id: measure.hqmf_set_id)
+    if existing.count > 1
+      measure.delete
+      render json: {status: "error", messages: "A measure with this HQMF Set ID already exists.", url: "/api_v1/measures/#{measure.hqmf_set_id}"},
+             status: :conflict
+    else
+      Measures::ADEHelper.update_if_ade(measure)
+      measure.generate_js
+      measure.save!
+
+      render json: {status: "success", url: "/api_v1/measures/#{measure.hqmf_set_id}"},
+             status: :ok
+    end
   end
 
   api :PUT, '/api_v1/measures/:id', 'Update an Existing Measure'
@@ -255,6 +371,11 @@ class ApiV1::MeasuresController < ApplicationController
   
   def error_parameter_missing(exception)
     render json: {status: "error", messages: "Missing parameter: #{exception.param}" },
+           status: :bad_request
+  end
+  
+  def error_parameter_invalid(exception)
+    render json: {status: "error", messages: "Invalid parameter '#{exception.param}': #{exception.error}" },
            status: :bad_request
   end
 
