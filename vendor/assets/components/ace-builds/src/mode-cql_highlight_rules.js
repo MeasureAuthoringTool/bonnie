edf4138c9e8fbad2fa42b349e93dd7312ba87d9f
holmesie <aholmes@mitre.org>
2016-06-01 13:24:14 -0400

    basic CQL syntax highlighting working

--- vendor/assets/components/ace-builds/src/mode-cql_highlight_rules.js
+++ vendor/assets/components/ace-builds/src/mode-cql_highlight_rules.js
@@ -1,10 +1,10 @@
-define("ace/mode/cql_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
+define('ace/mode/mode-cql_highlight_rules', [], function(require, exports, module) {
 "use strict";
 
 var oop = require("../lib/oop");
 var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 
-var CqlHighlightRules = function() {
+var CQLHighlightRules = function() {
 
  var keywords = (
             "library|version|using|include|called|public|private|parameter|default|valueset|"+
@@ -15,7 +15,7 @@ var CqlHighlightRules = function() {
             "months|days|minutes|hours|seconds|milliseconds|convert|to|start|end|of|duration|"+
             "width|successor|predecessor|singleton|minimum|maximum|if|then|else|div|mod|case|"+
             "collapse|expand|when|then|before|after|more|less|occurs|same|included|overlaps|"+
-            "display|Code|Concept|starts|ends"
+            "display|Code|Concept|starts|ends|during"
         );
 
         var langConstant = (
@@ -38,15 +38,11 @@ var CqlHighlightRules = function() {
         var rstart = "(\\[)"
         var spaces = "(\\s*)"
         var ret = rstart+spaces+model+spaces+"(\:)?"+spaces+"("+identifierReg+")?(\s+in)?"
-        console.log(ret)
         var stringEscape = /\\(?:x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|[0-2][0-7]{0,2}|3[0-6][0-7]?|37[0-7]?|[4-7][0-7]?|.)/;
         this.$rules = {
             "start" : [
-                {token : "quotedIdentifier", regex : /\"/, next  : "quotedIdentifier"},
+                {token : "variable", regex : /\".*?\"/},
                 {token : "string", regex : "'", next  : "qstring"},
-               // {token : "retrieve", regex : /\[.*\]/},
-                //{token : "dt", regex : /\@\T[0-9][0-9](\:[0-9][0-9])?/},
-                //{token : "date" , regex : /\@\T[0-9][0-9] (\:[0-9][0-9] (\:[0-9][0-9] ('.'[0-9]+)?)?)?(\Z | (('+' | '-') [0-9][0-9]\:[0-9][0-9]))?/},
                 {token : "doc.comment", regex : /\/\*.+/, next: "comment"},
                 {token : "comment",  regex : /\/\/.+$/},
                 {token : "invalid", regex: "\\.{2,}"},
@@ -60,40 +56,20 @@ var CqlHighlightRules = function() {
                 {token : "paren.lparen", regex : "[\\({]"},
                 {token : "paren.rparen", regex : "[\\)}]"},
                 {token : "constant.numeric", regex: "[+-]?\\d+\\b"},
-
                 {token : "variable.parameter", regex : /sy|pa?\d\d\d\d\|t\d\d\d\.|innnn/}, 
-                //{token : "keyword", regex : compoundKeywords}, 
                 {token : "variable.parameter", regex : /\w+-\w+(?:-\w+)*/}, 
                 {token : keywordMapper, regex : "\\b\\w+\\b"},
                 {caseInsensitive: true}
             ],
-            "qstring" : [
-                {token : "constant.language.escape",   regex : "''"},
-                {token : "string", regex : "'",     next  : "start"},
-                {defaultToken : "string"}
-            ],
-            "quotedIdentifier" : [
-
-                {token : "constant.language.escape",   regex :/\\\"/},
-                {token : "quotedIdentifier", regex : /\"/,next: "start"},
-                {defaultToken : "quotedIdentifier"}
-            ]
-            ,
             "comment" : [
                 {token : "doc.comment", regex : /\*\//,     next  : "start"},
                 {defaultToken : "doc.comment"}
-
-            ],
-
-            "libraryDef" : [
-                {token : "comment",  regex : /\/\/.+$/},
-                {token: ""}
             ]
         };
 
       this.normalizeRules();
     }
-oop.inherits(CqlHighlightRules, TextHighlightRules);
-exports.CqlHighlightRules = CqlHighlightRules;
+oop.inherits(CQLHighlightRules, TextHighlightRules);
+exports.CQLHighlightRules = CQLHighlightRules;
 
 });
