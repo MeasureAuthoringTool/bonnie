2de2674b8b706379dcef25536a0d028372983341
Matt Mayer <mmayer@mitre.org>
2016-08-08 09:42:58 -0400

    Remove old jasmine config files

--- spec/javascripts/spec.js
+++
@@ -1,7 +0,0 @@
-// Anything included here will be run through the asset pipeline.
-//= require_tree ./vendor
-//= require_tree ./helpers
-//= require_tree ./setup
-//= require_tree ./integration
-//= require_tree ./models
-//= require_tree ./views
