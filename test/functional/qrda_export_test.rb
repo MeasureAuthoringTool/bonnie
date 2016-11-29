be526d9cf83f41663b1c6ad58e7d3aa5f1e46ca1
Elliott <aelliott@mm198210-pc.mitre.org>
2016-11-29 09:17:05 -0500

    BONNIE-158 Removes non-determinate fields from comparison. Creates fixtures for tests.

--- test/functional/qrda_export_test.rb
+++ test/functional/qrda_export_test.rb
@@ -22,10 +22,20 @@ class RegistrationsControllerTest < ActionController::TestCase
         patient_dir = File.join("records", "qrda", test_dir)
         user_dir = File.join("users", "qrda", test_dir)
         collection_fixtures(user_dir, value_sets_dir, measure_dir, patient_dir)
-        
+
+        user = User.first
         patient = Record.first
         measure = Measure.first
-        
+
+        associate_user_with_measures(user, [measure])
+        associate_user_with_patients(user, [patient])
+        associate_measure_with_patients(measure, [patient])
+
+        HealthDataStandards::SVS::ValueSet.all.each do |vs|
+          vs.bundle_id = BSON::ObjectId.from_string(user.bundle_id)
+          vs.save
+        end
+
         start_time = Time.new(Time.zone.at(APP_CONFIG['measure_period_start']).year, 1, 1)
         end_time = Time.new(Time.zone.at(APP_CONFIG['measure_period_start']).year, 12, 31)
         
@@ -35,14 +45,21 @@ class RegistrationsControllerTest < ActionController::TestCase
         
         expected_file = Dir.glob(File.join("test", "fixtures", "qrda", test_dir, "*.xml"))[0]
         expected_xml = Nokogiri::XML(File.read(expected_file))
+
+        tags_to_remove = ["ClinicalDocument id", "ClinicalDocument effectiveTime", "ClinicalDocument time"]
+        tags_to_remove.each do |code|
+          gen_code = ret_xml.search(code).remove
+          ex_code = expected_xml.search(code).remove
+        end
+
         errors = []
         EquivalentXml.equivalent?(expected_xml, ret_xml, {:element_order => false, 
-          :normalize_whitespace => true, 
-          :ignore_content => ["ClinicalDocument realmCode",
-            "ClinicalDocument effectiveTime"]}) {|n1, n2, result| puts "#{n1.name} #{result}"; errors.push([n1, n2]) unless result}
+          :normalize_whitespace => true}) {|n1, n2, result| errors.push([n1.name, n1.values, n2.name, n2.values]) unless result;}
             
         
         errors.each do |err|
+          puts "#{err[0]} #{err[1]} #{err[2]} #{err[3]}"
+          total_errors.push(err)
         end
       end
       assert_equal [], total_errors, "QRDA Export Errors Found: #{total_errors.size}"
