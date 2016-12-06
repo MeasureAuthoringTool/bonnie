3b7935866a3d4ab6cf0d8264e2f456be172b8ea0
Matt Mayer <mmayer@mitre.org>
2016-12-06 18:10:23 -0500

    Refactor archived measures controller test and add test for not existant archived measure

--- test/functional/archived_measures_controller_test.rb
+++ test/functional/archived_measures_controller_test.rb
@@ -12,30 +12,33 @@ include Devise::TestHelpers
     users = File.join("users","base_set")
     collection_fixtures(draft_measures, users, archived_measures)
     @user = User.by_email('bonnie@example.com').first
-    @measure = Measure.where({"cms_id" => "CMS128v2"}).first
-    associate_user_with_measures(@user,[@measure])
+    @measure_without_archives = Measure.where({"cms_id" => "CMS128v2"}).first
+    @measure_with_archive = Measure.where({"cms_id" => "CMS104v2"}).first
+    @archived_measure = ArchivedMeasure.first
+    associate_archived_measures_with_measure([@archived_measure],@measure_with_archive)
+    associate_user_with_measures(@user,[@measure_with_archive,@archived_measure,@measure_without_archives])
     sign_in @user
   end
-  
+
   test "index of measure with no archived measures" do
-    get :index, { measure_id: @measure.id, format: :json }
+    get :index, { measure_id: @measure_without_archives.id, format: :json }
     assert_response :success
     assert_equal [], JSON.parse(response.body)
   end
-  
+
   test "index on non-existent measure" do
     get :index, { measure_id: "1234567890abcdef01234567", format: :json }
     assert_response :not_found
   end
-  
-  
-  test "show archived measure" do
-    measure_with_archive = Measure.where({"cms_id" => "CMS104v2"}).first
-    archived_measure = ArchivedMeasure.first
-    associate_archived_measures_with_measure([archived_measure],measure_with_archive)
-    associate_user_with_measures(@user,[measure_with_archive,archived_measure])
-    get :show, {measure_id: measure_with_archive.id, id: archived_measure.measure_db_id, format: :json}
+
+  test "show existing archived measure" do
+    get :show, {measure_id: @measure_with_archive.id, id: @archived_measure.measure_db_id, format: :json}
     assert_response :success
+  end
+  
+  test "show non existing archived measure" do
+    get :show, {measure_id: @measure_without_archives.id, id: "nonexistingmeasureid", format: :json}
+    assert_response :not_found
   end
   
 end
