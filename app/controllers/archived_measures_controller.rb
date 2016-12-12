ef2878d95632c67ba8a2b81a16ad47387dbc4f5f
hossenlopp <hossenlopp@mitre.org>
2016-12-12 13:41:18 -0500

    Measure History New Controllers Comments and Fixes (#615)
    * Added comments and addressed a few small issues.
     - Removed skip_before_action. This was bad copy pasting.
     - Made it so limited attr database calls include _id.
      - @mayerm94 showed me these being excluded are casusing silent errors that appear in tests.
    
    * Added comments to new routes and removed extraneous routes
    
    * Fixed incorrect comments in ArchivedMeasures and UploadSummaries controllers

--- app/controllers/archived_measures_controller.rb
+++ app/controllers/archived_measures_controller.rb
@@ -1,16 +1,25 @@
-# Methods for handling archived measures
+##
+# This controller provides access to the ArchivedMeasures. Backbone accesses these functions to lazy
+# load ArchivedMeasures.
+
 class ArchivedMeasuresController < ApplicationController
-  
-  skip_before_action :verify_authenticity_token, only: [:show, :value_sets]
   
   respond_to :json, :js, :html
   
-  # List the measure_db_id for all of versions of a measure using the
-  # hqmf_set_id as the measure identifier (so as to version agnostic)
+  ##
+  # GET /measures/:measure_id/archived_measures
+  #
+  # Lists the measure_db_id for all ArchivedMeasures for a specific measure. This is an index function that only returns
+  # a list of measure_db_ids.
+  
   def index
     begin
-      @measure = Measure.by_user(current_user).only(:hqmf_set_id).find(params[:measure_id])
-      @archived_measures = ArchivedMeasure.by_user(current_user).only(:measure_db_id).where(hqmf_set_id: @measure.hqmf_set_id)
+      # Fetch only hqmf_set_id and _id fields of the measure so we can use it to grab the archived measures for that 
+      # hqmf_set_id.
+      @measure = Measure.by_user(current_user).only(:hqmf_set_id, :_id).find(params[:measure_id])
+      
+      # Fetch only the measure_db_id and _id fields of the archived measures.
+      @archived_measures = ArchivedMeasure.by_user(current_user).only(:measure_db_id, :_id).where(hqmf_set_id: @measure.hqmf_set_id)
       
       respond_with @archived_measure do |format|
         format.json { render json: @archived_measures }
@@ -20,7 +29,11 @@ class ArchivedMeasuresController < ApplicationController
     end
   end
   
-  # Show the archived content of a measure at specific point in time
+  ##
+  # GET /measures/:measure_id/archived_measures/:id
+  #
+  # Gets a specific archived measure by its measure_db_id.
+  
   def show
     skippable_fields = [:map_fns, :record_ids, :measure_attributes]
     @archived_measure = ArchivedMeasure.by_user(current_user).where(measure_db_id: params[:id]).first
