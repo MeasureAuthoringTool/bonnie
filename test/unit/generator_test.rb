149b572577f7cf1cdd6366a0446e8396815e3947
David Connolly <dpc15038@gmail.com>
2017-04-12 08:51:11 -0400

    Improved Code Coverages (#562)
    * Improved coverage from 92.05% to 100%
    
    * Improved coverage from 88.89% to 100%
    
    * All 'grant_and_revoke' tests now check .is_approved?

--- test/unit/generator_test.rb
+++ test/unit/generator_test.rb
@@ -52,6 +52,10 @@ class GeneratorTest < ActiveSupport::TestCase
     assert_equal "conditions", HQMF::Generator.classify_entry(:resolvedDiagnoses)
     assert_equal "conditions", HQMF::Generator.classify_entry(:allProblems)
     assert_equal "medical_equipment", HQMF::Generator.classify_entry(:allDevices)
+    assert_equal "care_goals", HQMF::Generator.classify_entry(:careGoals)
+    assert_equal "family_history", HQMF::Generator.classify_entry(:family_history)
+    assert_equal "care_experiences", HQMF::Generator.classify_entry(:careExperiences)
+
 
     assert_equal "conditions", HQMF::Generator.classify_entry(:conditions)
     assert_equal "encounters", HQMF::Generator.classify_entry(:encounters)
