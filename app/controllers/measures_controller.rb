bceb3eb5260da13a0d6946dd0139df3fa9e996dd
Elliott <aelliott@mm198210-pc.mitre.org>
2017-07-14 17:50:03 -0400

    BONNIE-820 Add frontend exception handling of Cql translation errors.

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -164,6 +164,9 @@ class MeasuresController < ApplicationController
         elsif e.is_a? Measures::HQMFException
           operator_error = true
           flash[:error] = {title: "Error Loading Measure", summary: "Error loading XML file.", body: "There was an error loading the XML file you selected.  Please verify that the file you are uploading is an HQMF XML or SimpleXML file.  Message: #{e.message}"}
+        elsif e.is_a? Measures::ElmParsingException
+          operator_error = true
+          flash[:error] = {title: "Error Translating CQL to ELM", summary: "There was an error in parsing CQL into ELM.", body: "Please verify that cql being uploaded is properly formatted. #{e.message}"}
         elsif e.is_a? Measures::VSACException
           operator_error = true
           flash[:error] = {title: "Error Loading VSAC Value Sets", summary: "VSAC value sets could not be loaded.", body: "Please verify that you are using the correct VSAC username and password. #{e.message}"}
