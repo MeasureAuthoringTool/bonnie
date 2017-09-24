7c3e0a42d4526b93ed92680a59f716b92b87609b
Lizzie DeYoung <edeyoung@mitre.org>
2017-09-24 13:42:05 -0400

    re-added missing tests and updated to work with CQL

--- test/functional/measures_controller_test.rb
+++ test/functional/measures_controller_test.rb
@@ -176,7 +176,28 @@ include Devise::TestHelpers
     assert_response :success
     assert_equal 3, Measure.all.count
   end
-  
+
+  test "measure value sets" do
+    sign_in @user
+    measure_file = fixture_file_upload(File.join('test','fixtures', 'cql_measure_exports', 'IETCQL_v5_0_Artifacts.zip'), 'application/xml')
+    class << measure_file
+      attr_reader :tempfile
+    end
+
+    measure = nil
+    VCR.use_cassette("valid_vsac_response") do
+      # Assert measure is not yet loaded
+      post :create, {vsac_date: '08/22/2017', include_draft: true, measure_file: measure_file, measure_type: 'ep', calculation_type: 'patient', vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD']}
+    end
+
+    assert_response :redirect
+    measure = CqlMeasure.where({hqmf_id: "40280582-5859-673B-0158-DAEF8B750647"}).first
+
+    get :value_sets, {id: measure.id, format: :json}
+    assert_response :success
+    assert_equal 15, JSON.parse(response.body).keys.count
+  end
+
   test "upload invalid file format" do
     measure_file = fixture_file_upload(File.join('test', 'fixtures', 'measure_exports', 'measure_invalid_extension.foo'), 'application/zip')
     class << measure_file
@@ -311,11 +332,150 @@ include Devise::TestHelpers
     measure = CqlMeasure.where({hqmf_id: "40280582-5859-673B-0158-DAEF8B750647"}).first
     assert_equal "762B1B52-40BF-4596-B34F-4963188E7FF7", measure.hqmf_set_id
   end
-  
-  test "debug test" do
-    get :debug, {id: @measure.id}
-    assert_response :success
+
+  test "upload missing value set missing oid" do
+    # in the CQL file, the 'Alcohol and Drug Dependence Treatment' value set oid was changed from
+    # '2.16.840.1.113883.3.464.1003.106.12.1005' to '2.16.840.1.113883.3.464.1003.106.12.1001'.
+    # no changes in the HQMF.
+    sign_in @user
+    measure_file = fixture_file_upload(File.join('test','fixtures', 'cql_measure_exports', 'IETCQL_v5_0_missing_vs_oid_Artifacts.zip'), 'application/xml')
+    class << measure_file
+      attr_reader :tempfile
+    end
+
+    measure = nil
+    VCR.use_cassette("missing_vs_oid_response") do
+      post :create, {vsac_date: '09/24/2017', include_draft: true, measure_file: measure_file, measure_type: 'ep', calculation_type: 'patient', vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD']}
+    end
+
+    assert_equal "Measure is missing value sets",flash[:error][:title]
+    assert_equal "The measure you have tried to load is missing value sets.", flash[:error][:summary]
+    assert_equal "The measure you are trying to load is missing value sets.  Try re-packaging and re-exporting the measure from the Measure Authoring Tool.  The following value sets are missing: [2.16.840.1.113883.3.464.1003.106.12.1005]", flash[:error][:body]
+
   end
 
+  test "create/finalize/update a measure" do
+    sign_in @user
+    measure_file = fixture_file_upload(File.join('test','fixtures', 'cql_measure_exports', 'IETCQL_v5_0_initial_Artifacts.zip'), 'application/xml')
+    class << measure_file
+      attr_reader :tempfile
+    end
+    update_measure_file = fixture_file_upload(File.join('test','fixtures', 'cql_measure_exports', 'IETCQL_v5_0_updates_Artifacts.zip'), 'application/xml')
+    class << update_measure_file
+      attr_reader :tempfile
+    end
+
+    measure = nil
+    VCR.use_cassette("initial_response") do
+      post :create, {vsac_date: '09/24/2017', include_draft: false, measure_file: measure_file, measure_type: 'eh', calculation_type: 'episode', vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD']}
+    end
+
+    assert_response :redirect
+    measure = CqlMeasure.where({hqmf_id: "40280582-5859-673B-0158-DAEF8B750647"}).first
+
+    assert_equal "762B1B52-40BF-4596-B34F-4963188E7FF7", measure.hqmf_set_id
+    assert_equal 15, measure.value_sets.count
+    assert_equal @user.id, measure.user_id
+    measure.value_sets.each {|vs| assert_equal @user.id, vs.user_id}
+    assert_equal true, measure.needs_finalize
+    assert_equal true, measure.episode_of_care?
+    assert_equal 'eh', measure.type
+    assert_nil measure.episode_ids
+
+    assert_equal 1, (measure.value_sets.select {|vs| vs.oid == '2.16.840.1.113883.3.464.1003.106.12.1001'}).count
+    assert_equal 745, (measure.value_sets.select {|vs| vs.oid == '2.16.840.1.113883.3.464.1003.106.12.1001' && vs.version == "MU2 Update 2017-01-06"}).first.concepts.count
+
+    # Finalize the measure that was just created
+    post :finalize, {"t679"=>{"hqmf_id"=>"40280582-5859-673B-0158-DAEF8B750647"}}
+    measure = CqlMeasure.where({hqmf_id: "40280582-5859-673B-0158-DAEF8B750647"}).first
+
+    assert_equal "762B1B52-40BF-4596-B34F-4963188E7FF7", measure.hqmf_set_id
+    assert_equal 15, measure.value_sets.count
+    assert_equal @user.id, measure.user_id
+    measure.value_sets.each {|vs| assert_equal @user.id, vs.user_id}
+    assert_equal false, measure.needs_finalize
+    assert_equal true, measure.episode_of_care?
+    assert_equal 'eh', measure.type
+    assert_nil measure.episode_ids
+
+    # Update the measure
+    VCR.use_cassette("update_response") do
+      post :create, {vsac_date: '09/24/2017', include_draft: false, measure_file: update_measure_file, measure_type: 'eh', calculation_type: 'episode', vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD']}
+    end
+
+    assert_response :redirect
+    measure = CqlMeasure.where({hqmf_id: "40280582-5859-673B-0158-DAEF8B750647"}).first
+
+    assert_equal "762B1B52-40BF-4596-B34F-4963188E7FF7", measure.hqmf_set_id
+    assert_equal 26, measure.value_sets.count # new entries for VSs with new versions, which is 11 of them
+    assert_equal @user.id, measure.user_id
+    measure.value_sets.each {|vs| assert_equal @user.id, vs.user_id}
+    assert_equal false, measure.needs_finalize
+    assert_equal true, measure.episode_of_care?
+    assert_equal 'eh', measure.type
+    assert_nil measure.episode_ids
+
+    assert_equal 2, (measure.value_sets.select {|vs| vs.oid == '2.16.840.1.113883.3.464.1003.106.12.1001'}).count
+    assert_equal 745, (measure.value_sets.select {|vs| vs.oid == '2.16.840.1.113883.3.464.1003.106.12.1001' && vs.version == "MU2 Update 2017-01-06"}).first.concepts.count
+    assert_equal 516, (measure.value_sets.select {|vs| vs.oid == '2.16.840.1.113883.3.464.1003.106.12.1001' && vs.version == "eCQM Update 2017-05-05"}).first.concepts.count
+  end
+
+  test "load HQMF bad xml" do
+    sign_in @user
+    measure_file = fixture_file_upload(File.join('test','fixtures', 'cql_measure_exports', 'IETCQL_v5_0_bad_hqmf_Artifacts.zip'), 'application/xml')
+    class << measure_file
+      attr_reader :tempfile
+    end
+
+    measure = nil
+    VCR.use_cassette("valid_vsac_response") do
+      # Assert measure is not yet loaded
+      post :create, {vsac_date: '08/22/2017', include_draft: true, measure_file: measure_file, measure_type: 'ep', calculation_type: 'patient', vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD']}
+    end
+
+    assert_response :redirect
+    measure = CqlMeasure.where({hqmf_id: "40280582-5859-673B-0158-DAEF8B750647"}).first
+
+    assert_nil measure
+    assert_includes flash[:error].keys, :title
+    assert_includes flash[:error].keys, :summary
+    assert_includes flash[:error].keys, :body
+    assert_equal 'The measure could not be loaded.', flash[:error][:summary]
+    flash.clear
+
+    assert_equal 2, Dir.glob(File.join(@error_dir, '**')).count
+    assert_equal true, FileUtils.identical?(File.join(@error_dir, (Dir.entries(@error_dir).select { |f| f.end_with?('.zip') })[0]), File.join('test', 'fixtures', 'cql_measure_exports', 'IETCQL_v5_0_bad_hqmf_Artifacts.zip'))
+  end
+
+  test "update a patient based measure" do
+    sign_in @user
+    measure_file = fixture_file_upload(File.join('test','fixtures', 'cql_measure_exports', 'IETCQL_v5_0_Artifacts.zip'), 'application/xml')
+    class << measure_file
+      attr_reader :tempfile
+    end
+
+    measure = nil
+    VCR.use_cassette("valid_vsac_response") do
+      post :create, {vsac_date: '08/22/2017', include_draft: true, measure_file: measure_file, measure_type: 'ep', calculation_type: 'patient', vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD']}
+    end
+
+    assert_response :redirect
+    measure = CqlMeasure.where({hqmf_id: "40280582-5859-673B-0158-DAEF8B750647"}).first
+    assert_equal "762B1B52-40BF-4596-B34F-4963188E7FF7", measure.hqmf_set_id
+
+    assert_equal false, measure.episode_of_care?
+    assert_equal 'ep', measure.type
+
+    VCR.use_cassette("valid_vsac_response") do
+      post :create, {measure_file: measure_file, hqmf_set_id: measure.hqmf_set_id}
+    end
+
+    assert_response :redirect
+    measure = CqlMeasure.where({hqmf_id: "40280582-5859-673B-0158-DAEF8B750647"}).first
+    assert_equal "762B1B52-40BF-4596-B34F-4963188E7FF7", measure.hqmf_set_id
+
+    assert_equal false, measure.episode_of_care?
+    assert_equal 'ep', measure.type
+  end
 
 end
