c8d03d72a769c10bfa8c3f554d34387f09263d1c
Elliott <aelliott@mm198210-pc.mitre.org>
2017-10-18 10:19:47 -0400

    TEST

--- lib/ext/concept.rb
+++ lib/ext/concept.rb
@@ -1,6 +1,7 @@
 module HealthDataStandards
   module SVS
     class Concept
+      #tst
       include Mongoid::Document
       field :white_list, type: Boolean, default: false
       field :black_list, type: Boolean, default: false
