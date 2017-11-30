b220055b4df0e3dde4fc2d95c53ec1b4e361c3f1
Luke Osborne <luke.w.osborne@gmail.com>
2017-11-30 13:00:09 -0500

    Master cherry picks (#907)
    * added 404 page.
    
    * removed use of 'waitsForAndRuns', like https://github.com/projecttacoma/bonnie/pull/671
    
    * made blue bunny not have mangled name for production (#673)
    
    * Add XFS Protection (#810)
    
    * Fixed rebuilding patients on upload; make rebuild patients happen for every upload (#687)
    
    * add unit test to cover rebuild_patients in measures_controller
    
    * add front end tests to cover 404 page code

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -196,12 +196,10 @@ class MeasuresController < ApplicationController
     end
     measure.save!
 
-    # rebuild the users patients if set to do so
-    if params[:rebuild_patients] == "true"
-      Record.by_user(current_user).each do |r|
-        Measures::PatientBuilder.rebuild_patient(r)
-        r.save!
-      end
+    # rebuild the user's patients for the given measure
+    Record.by_user_and_hqmf_set_id(current_user, measure.hqmf_set_id).each do |r|
+      Measures::PatientBuilder.rebuild_patient(r)
+      r.save!
     end
 
     # ensure expected values on patient match those in the measure's populations
