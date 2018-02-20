3ac1ba07271caeb64f317341a5bae8f2ee13adbb
Adam M. Holmes <aholmes@mitre.org>
2018-02-20 13:06:58 -0500

    Bonnie prior no hard coded emails (#930)
    * Removed hard coded email address; Made email config (email.yml) server specific
    
    * Added comment explaining bonnie_email field in config/bonnie.yml
    
    * Added user mailer test
    
    * Added second email configuration for exception emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'bonnie-feedback-list@lists.mitre.org'
+  config.mailer_sender = APP_CONFIG['bonnie_email']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
