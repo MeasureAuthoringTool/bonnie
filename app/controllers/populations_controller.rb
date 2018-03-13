226ff3ee1508ae1c97ea99313f33bba448844a52
jbradl11 <james.h.bradley13@gmail.com>
2018-03-13 11:55:27 -0400

    Initial code removal of unused backend code (#958)

--- app/controllers/populations_controller.rb
+++ app/controllers/populations_controller.rb
@@ -1,15 +1,5 @@
 class PopulationsController < ApplicationController
 
-  skip_before_action :verify_authenticity_token, only: :calculate_code
-
-  def calculate_code
-    measure = Measure.by_user(current_user).find(params[:measure_id])
-    if stale? last_modified: measure.updated_at.try(:utc), etag: measure.cache_key
-      options = params[:rationale] == 'false' ? { rationale: false } : { } # Only pass rationale if explicitly false
-      render js: BonnieMeasureJavascript.generate_for_population(measure, params[:id].to_i, options)
-    end
-  end
-
   def update
     # Added CQL Based Measure support for editing Population titles.
     # If Measure doesn't exist in Measures move on to CqlMeasures.
