095dcebf12ef9bc71fcb5086345a863fa108f30b
Matt Mayer <mmayer@mitre.org>
2018-06-18 12:43:17 -0400

    Remove whitespace

--- test/unit/helpers/patient_helper_test.rb
+++ test/unit/helpers/patient_helper_test.rb
@@ -7,18 +7,18 @@ class PatientHelperTest < ActionView::TestCase
     qdm_records = PatientHelper.convert_patient_models([])
     assert_equal [], qdm_records
   end
-  
+
   test 'Correct Number Of Patients Recieved From convert_patient_models' do
     records_set = File.join("records","base_set")
     hds_records = Record.all
     qdm_records = PatientHelper.convert_patient_models(hds_records)
     assert_equal hds_records.count, qdm_records.count
   end
-  
+
   test 'Calculations Are Unaffected By HDS to QDM Conversion' do
     skip('TODO Implement this test once backend calculator is implemented')
     # Run backend calculation on patient before and after conversion
     # Assert calculations are equal
   end
-  
+
 end
