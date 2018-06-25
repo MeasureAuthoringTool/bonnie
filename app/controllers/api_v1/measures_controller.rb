66dbbd226d143dbc2b529b0e1fa914041f9d8d52
jbradl11 <jhbradley@mitre.org>
2018-06-25 08:26:38 -0400

    Mat api cql (#1012)
    * Auto-generated MAT integration API v1 controller.
    
    * Scaffolding for getting, searching, evaluating, and creating/updating measures.
    
    * Remove useless auto-generated cruft.
    
    * Whitelist API attributes and select by HQMF SET ID.
    
    * Add apipie documentation to MAT API.
    
    * Partial implementation and tests of create.
    
    * Initial implementation of create API call.
    
    * Added more tests and started update api call.
    
    * Made calculation_type required again.
    
    * Secure MAT Integration API with OAuth2.
    
    * Updated to latest version of apipie from master.
    
    * Added xml measure upload to mat api. Added webmock and vcr gems to mock VSAC.
     - To rerecord VSAC calls:
       - Delete yml files in test/fixtures/vcr_cassettes
       - Run tests with VSAC_USERNAME=xxx and VSAC_PASSWORD=xxx in the command
    
    * Fixed issue with wrong user object being used. Fixed parameter list for update operation.
    
    * Now recording multipart formdata for with Apipie (pending a pull request).
    
    * Updated apipie refrence to hossenlopp's fork
    
    * Updated gemfile.lock for latest hossenlopp/apipie. Fixed apipie config to ignore non-api calls when recording examples.
    
    * Post rebase API examples update
    
    * Moved require of vcr_setup out of the controller and into the test_helper.
    
    * Updated VCR cassettes for VSAC updates to profile used.
    
    * added instructions for how to use the api to the staging environment.
    
    * fixed capitalization issue with the api guide file name.
    
    * Removed extraneous routes for MAT API. This fixed improper paths in the examples.
    
    * updated doorkeeper version
    
    * switched mongoid to not raise errors for not finding results
    
    * fixed oauth flow when user is logged out. stored location for Devise to redirect to after login
    
    * lowered lifespan of access_token, enabled refresh_token, disabled ROPC flow
    
    * now using vsac ticket granting ticket in api
    
    * Bonnie 1469: Add user info API call with username and email (#1006)
    
    * override doorkeeper token info endpoint
    
    * added tests and fix namespace error
    
    * checks user existence in token info endpoint
    
    * split out first and last name
    
    * Mat integration review dummy excel (#1007)
    
    * respond with dummy excel file for MAT api
    
    * add dummy excel file
    
    * verify excel contents in test
    
    * make xlsx default response
    
    * check for accept header rather than using respond_with
    
    * Initial integration work
    
    * Modified tests and token info controller due to mongoid.yml update
    
    * Updated tests, removed unused fixtures
    
    * Removed unused code, updated tests.
    
    * Fixed rubocop errors.
    
    * Modified failing test.
    
    * Modified logic syntax
    
    * Re-wrote config parameter
    
    * Set raise_not_found_error to false and modified rake tasks to handle this change.

---
+++ app/controllers/api_v1/measures_controller.rb
@@ -0,0 +1,171 @@
+module ApiV1
+  class MeasuresController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    skip_before_filter :authenticate_user!
+    before_action :doorkeeper_authorize! # Require access token for all actions
+
+    MEASURE_WHITELIST = %w[id cms_id complexity continuous_variable created_at description episode_of_care hqmf_id hqmf_set_id hqmf_version_number title type updated_at].freeze
+    PATIENT_WHITELIST = %w[_id birthdate created_at deathdate description ethnicity expected_values expired first gender insurance_providers last notes race updated_at].freeze
+    INSURANCE_WHITELIST = %w[member_id payer].freeze
+    POPULATION_TYPES = %w[population_index STRAT IPP DENOM NUMER DENEXCEP DENEX MSRPOPL OBSERV MSRPOPLEX].freeze
+
+    respond_to :json, :html, :xlsx
+    rescue_from Apipie::ParamMissing, :with => :error_parameter_missing
+    rescue_from ActionController::ParameterMissing, :with => :error_parameter_missing
+    rescue_from Apipie::ParamInvalid, :with => :error_parameter_invalid
+
+    resource_description do
+      formats [:json]
+      api_versions '1'
+      error :code => 401, :desc => 'Unauthorized'
+      description <<-DESCRIPTION
+        This resource allows access to `Measures`, `Patients`, and calculated
+        results for a given user.
+      DESCRIPTION
+    end
+
+    def_param_group :measure do
+      param :id, String, :required => true, :desc => 'The HQMF Set ID of the Measure.'
+      error :code => 404, :desc => 'Not Found'
+    end
+
+    # TODO: Will be uncommented when we add CQL Measure Upload support.
+    # def_param_group :measure_upload do
+    #   param :measure_file, File, :required => true, :desc => "The measure file."
+    #   param :measure_type, %w[eh ep], :required => true, :desc => "The type of the measure."
+    #   param :calculation_type, %w[episode patient], :required => true, :desc => "The type of calculation."
+    #   param :episode_of_care, Integer, :required => false, :desc => "The index of the episode of care. Defaults to 0. This means that the first specific occurence in the logic will be used for the episode of care calculation."
+    #   param :population_titles, Array, of: String, :required => false, :desc => "The titles of the populations. If this is not included, populations will assume default values. i.e. \"Population 1\", \"Population 2\", etc."
+    #   param :vsac_tgt, String, :required => false, :desc => "VSAC ticket granting ticket. Required when uploading a HQMF .xml measure."
+    #   param :vsac_tgt_expires_at, Integer, :required => false, :desc => "VSAC ticket granting ticket expiration time in seconds since epoch. Required when uploading a HQMF .xml measure."
+    #   param :include_draft, Boolean, :required => false, :desc => "If VSAC should fetch draft value sets. Defaults to 'true' if not supplied."
+    #   param :vsac_date, Date, :required => false, :desc => "The lastest effective date for published value sets to retrieve. Required when include_draft is false."
+    # end
+
+    api :GET, '/api_v1/measures', 'List of Measures'
+    description 'Retrieve the list of measures for the authorized user.'
+    formats [:json, :html]
+    def index
+      # TODO: filter by search parameters, for example an NQF ID or partial description
+      @api_v1_measures = CqlMeasure.by_user(current_resource_owner).only(MEASURE_WHITELIST)
+      respond_with @api_v1_measures do |format|
+        format.json do
+          render json: MultiJson.encode(
+            @api_v1_measures.map do |x|
+              h = x
+              h[:id] = x.hqmf_set_id
+              h
+            end
+          )
+        end
+        format.html { render :layout => false }
+      end
+    end
+
+    api :GET, '/api_v1/measures/:id', 'Read a Specific Measure'
+    description 'Retrieve the details of a specific measure by HQMF Set ID.'
+    param_group :measure
+    def show
+      hash = {}
+      http_status = 200
+      begin
+        @api_v1_measure = CqlMeasure.by_user(current_resource_owner).where({:hqmf_set_id=> params[:id]}).sort_by { :updated_at }.first
+        hash = @api_v1_measure.as_json
+        hash[:id] = @api_v1_measure.hqmf_set_id
+        hash.select! { |key,value| MEASURE_WHITELIST.include?(key)&&!value.nil? }
+      rescue StandardError
+        http_status = 404
+        hash = {}
+      end
+      render json: hash, status: http_status
+    end
+
+    api :GET, '/api_v1/measures/:id/patients', 'List of Patients for a Specific Measure'
+    description 'Get all the patients associated with a measure.'
+    param_group :measure
+    def patients
+      @api_v1_patients = []
+      http_status = 200
+      begin
+        # Get the measure
+        @api_v1_measure = CqlMeasure.by_user(current_resource_owner).where({:hqmf_set_id=> params[:id]}).sort_by(&:updated_at).first
+        # Extract out the HQMF set id, which we'll use to get related patients
+        hqmf_set_id = @api_v1_measure.hqmf_set_id
+        # Get the patients for this measure
+        @api_v1_patients = Record.by_user(current_resource_owner).where({:measure_ids.in => [hqmf_set_id]})
+        @api_v1_patients = process_patient_records(@api_v1_patients)
+      rescue StandardError
+        http_status = 404
+        @api_v1_patients = []
+      end
+      render json: @api_v1_patients, status: http_status
+    end
+
+    api :GET, '/api_v1/measures/:id/calculated_results', 'Calculated Results for a Specific Measure'
+    description 'Retrieve the calculated results of the measure logic for each patient.'
+    param_group :measure
+    error :code => 500, :desc => 'Server-side Error Calculating the HQMF Measure Logic'
+    formats [:json, :xlsx]
+    def calculated_results
+      begin
+        http_status = 200
+        # Get the measure
+        @api_v1_measure = CqlMeasure.by_user(current_resource_owner).where({:hqmf_set_id=> params[:id]}).sort_by(&:updated_at).first
+        # Extract out the HQMF set id, which we'll use to get related patients
+        hqmf_set_id = @api_v1_measure.hqmf_set_id
+        # Get the patients for this measure
+        @api_v1_patients = Record.by_user(current_resource_owner).where({:measure_ids.in => [hqmf_set_id]})
+      rescue StandardError
+        http_status = 404
+      end
+
+      if request.headers['Accept'] == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        if http_status != 404
+          filename = 'Sample_Excel_Export(CMS52v6).xlsx'
+          send_file "#{Rails.root}/public/resource/#{filename}", type: :xlsx, status: http_status, filename: ERB::Util.url_encode(filename)
+        end
+      else
+        render json: {status: "error", messages: "Unimplemented functionality"}, status: :bad_request
+      end
+    end
+
+    private
+
+    def process_patient_records(selector)
+      patients = selector.map(&:as_json)
+      patients.each do |p|
+        p.select! { |key,value| PATIENT_WHITELIST.include?(key) && !value.nil? }
+        unless p["insurance_providers"].nil?
+          p["insurance_providers"].map! do |hash|
+            hash.select! { |k,v| INSURANCE_WHITELIST.include?(k) && !v.nil? }
+            hash["payer"] = hash["payer"]["name"] if hash["payer"]
+            hash
+          end
+        end
+        unless p["expected_values"].nil?
+          p["expected_values"].map! do |hash|
+            hash.select! { |k,v| POPULATION_TYPES.include?(k) && !v.nil? }
+          end
+        end
+        p["birthdate"] = Time.at(p["birthdate"]).iso8601 if p["birthdate"]
+        p["deathdate"] = Time.at(p["deathdate"]).iso8601 if p["deathdate"]
+      end
+      patients
+    end
+
+    def error_parameter_missing(exception)
+      param_name = exception.is_a?(Apipie::ParamMissing) ? exception.param.name : exception.param
+      render json: {status: "error", messages: "Missing parameter: #{param_name}" }, status: :bad_request
+    end
+    
+    def error_parameter_invalid(exception)
+      render json: {status: "error", messages: "Invalid parameter '#{exception.param}': #{exception.error.gsub(%r{/<\/?code>/}, '')}" },
+             status: :bad_request
+    end
+
+    def current_resource_owner
+      User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    end
+
+  end
+end
