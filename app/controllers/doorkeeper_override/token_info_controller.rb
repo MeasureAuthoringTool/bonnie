07fd9f35556245a7e8fade489ed81e08acb48a66
hossenlopp <hossenlopp@mitre.org>
2018-06-29 11:24:23 -0400

    Doorkeeper refresh_token expiration BONNIE-1571 (#1025)
    * added patch to doorkeeper to support expiration of refresh_tokens
    
    * made rubocop happy
    
    * moved token expiration time calculation to model class and stopped using a stub token for token info unit test

--- app/controllers/doorkeeper_override/token_info_controller.rb
+++ app/controllers/doorkeeper_override/token_info_controller.rb
@@ -12,6 +12,13 @@ module DoorkeeperOverride
             expires_in_seconds: doorkeeper_token.expires_in_seconds,
             created_at: doorkeeper_token.created_at.to_i
           }
+          # if the refresh token will expire, add the time until it expires
+          if doorkeeper_token.refresh_token_expiration_time
+            # if the original_token_created_at time exists determine expiration time from that, otherwise use created_at time.
+            token_info[:refresh_expires_in_seconds] = (doorkeeper_token.refresh_token_expiration_time - Time.now).to_i
+            # if the refresh token has already expired, show zero for the time to expiration
+            token_info[:refresh_expires_in_seconds] = 0 if (token_info[:refresh_expires_in_seconds]).negative?
+          end
           render json: token_info, status: :ok
         else # If the user does not exist, then return with unauthorized error response
           error = Doorkeeper::OAuth::ErrorResponse.new(name: :unauthorized)
