32854b6bfea4db2b5c4c5513ca26ea31808ab573
daco101 <40396363+daco101@users.noreply.github.com>
2018-07-06 13:03:49 -0400

    Bonnie v2.3 restful calculation call (#1028)
    * added restful call to cql calc, slight mod to test_helper.rb for valuesets
    
    * for rubocop
    
    * now uses qdm patient converter
    
    * fix spelling
    
    * added query ordering so results are consistent and work with vcr
    
    * fixed array ordering for vcr
    
    * added timeout and connection refused handling and tests

--- lib/bonnie_backend_calculator.rb
+++ lib/bonnie_backend_calculator.rb
@@ -1,34 +1,51 @@
-# Calculate measures from ruby, using V8; use it something like this:
-#
-#   calculator = BonnieBackendCalculator.new
-#   calculator.set_measure_and_population(measure1, 0)
-#   calculator.calculate(patient1)
-#   calculator.calculate(patient2)
-#   calculator.set_measure_and_population(measure2, 0)
-#   calculator.calculate(patient3)
-#   calculator.calculate(patient4)
 
-class BonnieBackendCalculator
+# Do the measure calculation using the restful calculation microservice, 
+# will convert patients to QDM model prior to calculation.
+module BonnieBackendCalculator
+  CALCULATION_SERVICE_URL = 'http://localhost:8081/calculate'.freeze
 
-  def initialize
-    # Set up the V8 context and evaluate the basic libraries
-    @v8 = V8::Context.new
-    @v8.eval HQMF2JS::Generator::JS.map_reduce_utils
-    # Don't include crosswalk but do include underscore when loading library functions
-    @v8.eval HQMF2JS::Generator::JS.library_functions(false, true)
+  def self.calculate(measure, patients, value_sets, options)
+    # convert patients to QDM, note that once we switch to the QDM model this will become unnecessary (or maybe optional)
+    qdm_patients, failed_patients = PatientHelper.convert_patient_models(patients)
+
+    post_data = {
+      patients: qdm_patients,
+      measure: measure,
+      valueSets: value_sets,
+      options: options
+    }
+    begin
+      response = RestClient.post(CALCULATION_SERVICE_URL,
+                                 post_data.to_json,
+                                 content_type: 'application/json')
+      results = JSON.parse(response)
+
+      # add back the failed patients, TODO: discuss this strategy
+      results["patients"].push(*failed_patients)
+      
+      return results
+    rescue RestClient::Exception => e
+      raise self::RestException.new(e.message)
+    rescue Errno::ECONNREFUSED
+      raise self::RestException.new("Server refused connection on that port. Is the service running?")
+    rescue JSON::ParserError
+      raise self::ResponseException.new("JSON parse error")
+    end
   end
 
-  # Specify the measure and population to calculate against; can be called multiple times to change
-  def set_measure_and_population(measure, population, options = {})
-    options.reverse_merge! rationale: false # Don't generate rationale by default (more expensive)
-    options.reverse_merge! clear_db_cache: false # Don't regenerate the JavaScript by default
-    @v8.eval BonnieBackendMeasureJavascript.generate_for_population(measure, population, options)
+  # Generic Backend Calculator related exception.
+  class BackendCalculatorError < StandardError
   end
-
-  # Calculate a patient against the previously set up measure and population, returning the result
-  def calculate(patient)
-    result = @v8.eval "calculatePatient(#{patient.to_json(except: :results)})"
-    JSON.parse(result)
+  # Error representing a problem with the rest call
+  class RestException < BackendCalculatorError
+    def initialize(message)
+      super("Problem with the rest call to the calculation microservice: #{message}.")
+    end
   end
-
+  # Problem with the response from the calculation service
+  class ResponseException < BackendCalculatorError
+    def initialize(problem_description)
+      super("Problem with the response from the calculation microservice: #{problem_description}.")
+    end
+  end
 end
---
+++ test/unit/backend_calc_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+require 'vcr_setup.rb'
+require 'webmock'
+
+WebMock.enable!
+
+class BonnieBackendCalculatorTest < ActiveSupport::TestCase
+
+  include WebMock::API
+
+  setup do
+    dump_database
+    measures_set = File.join("cql_measures", "core_measures", "CMS160v6")
+    records_set = File.join("records", "core_measures", "CMS160v6")
+    value_sets = File.join("health_data_standards_svs_value_sets", "core_measures", "CMS160v6")
+    collection_fixtures(measures_set, records_set, value_sets)
+  end
+
+  # This test will very likely need to be changed once the calculation service is working, 
+  # right now the service just echoes, so this test makes sure the parsing is working etc
+  test "echo test" do
+    VCR.use_cassette('backend_calculator_echo_test') do
+      measure = CqlMeasure.order_by(:id => 'asc').first # we order_by to make sure we pull the same measure across runs
+      patients = Record.where('measure_ids'=>{'$in'=>[measure.hqmf_set_id]})
+      value_sets = measure.value_sets
+      options = {
+        prettyPrint: true
+      }
+      r = BonnieBackendCalculator.calculate(measure, patients, value_sets, options)
+
+      assert_equal measure.to_json, r['measure'].to_json
+      assert_equal options.to_json, r['options'].to_json
+      
+      # since we might get value sets and patients in different order from mongo across runs, check that some sorted properties match
+      assert_equal value_sets.collect(&:oid).sort, r['valueSets'].collect { |vs| vs['oid'] }.sort
+      assert_equal value_sets.collect(&:display_name).sort, r['valueSets'].collect { |vs| vs['display_name'] }.sort
+
+      # note patients are converted so field names change
+      assert_equal patients.collect(&:first).sort, r['patients'].collect { |p| p['givenNames'][0] }.sort
+      assert_equal patients.collect(&:last).sort, r['patients'].collect { |p| p['familyName'] }.sort
+    end
+  end
+
+  test "timeout test" do
+    assert_raise BonnieBackendCalculator::RestException do
+      stub_request(:post, BonnieBackendCalculator::CALCULATION_SERVICE_URL).to_timeout
+      BonnieBackendCalculator.calculate(nil, [], [], nil)
+    end
+  end
+
+  # if the server is running but the service is not, then the server will refuse the connection on that port and you will get an error as follows
+  test "service down test" do
+    assert_raise BonnieBackendCalculator::RestException do
+      stub_request(:post, BonnieBackendCalculator::CALCULATION_SERVICE_URL).to_raise(Errno::ECONNREFUSED)
+      BonnieBackendCalculator.calculate(nil, [], [], nil)
+    end
+  end
+
+end
