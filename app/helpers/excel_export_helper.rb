81306807dd80fe111f0382df5bf892a3ea4fdd40
Luke Osborne <lwosborne@mitre.org>
2018-07-10 14:06:20 -0400

    complete work on excel_export_helper functions

--- app/helpers/excel_export_helper.rb
+++ app/helpers/excel_export_helper.rb
@@ -1,30 +1,51 @@
 require 'uri'
 
 module ExcelExportHelper
-  def self.convert_results_for_excel_export(results)
+  def self.convert_results_for_excel_export(results, measure, patients)
     # Convert results from the back-end calculator to the format
     # expected the excel export module
 
-    results_for_excel_export = {}
-    # TODO: do conversion here
+    calc_results = {}
+    measure.populations.each_with_index do | population, index |
+      patients.each do | patient |
+        if !calc_results[index]
+          calc_results[index] = {}
+        end
+        result_criteria = {}
+        result = results[patient.id]
+        result[population['id']]['extendedData']['population_relevance'].keys.each do |pop_crit|
+          if pop_crit == 'values'
+            values = []
+            # gather the values from the episode_results object.
+            result[population['id']]['episode_results'].each do | index, episode |
+              values.concat episode['values']
+            end
+            result_criteria[pop_crit] = values
+          else
+            result_criteria[pop_crit] = result[population['id']][pop_crit]
+          end
+        end
+        calc_results[index][patient.id] = {statement_results: remove_extra(result[population['id']]['statement_results']), criteria: result_criteria}
 
-    ####################################################################################
-    # These are snippets from the existing conversion in measure_view.js.coffee
-    ####################################################################################
-    # for pop in @model.get('populations').models
-    #   for patient in @model.get('patients').models
-    #     if calc_results[pop.cid] == undefined
-    #       calc_results[pop.cid] = {}
-    #     # Re-calculate results before excel export (we need to include pretty result generation)
-    #     bonnie.calculator_selector.clearResult pop, patient
-    #     result = pop.calculate(patient, {doPretty: true})
-    #     result_criteria = {}
-    #     for pop_crit of result.get('population_relevance')
-    #       result_criteria[pop_crit] = result.get(pop_crit)
-    #     calc_results[pop.cid][patient.cid] = {statement_results: @removeExtra(result.get("statement_results")), criteria: result_criteria}
-    ####################################################################################
-    # TODO: do any other conversion needed
-    return results_for_excel_export
+      end
+    end
+
+    return calc_results.with_indifferent_access
+  end
+
+  private_class_method def self.remove_extra(results)
+    ret = {}
+    results.each_key do |libKey|
+      ret[libKey] = {}
+      results[libKey].each_key do |statementKey|
+        if results[libKey][statementKey]['pretty']
+          ret[libKey][statementKey] = results[libKey][statementKey]['pretty']
+        else
+          ret[libKey][statementKey] = results[libKey][statementKey]['final']
+        end
+      end
+    end
+    return ret
   end
 
   def self.get_patient_details(patients)
@@ -45,47 +66,27 @@ module ExcelExportHelper
         }
       end
     end
-    return patient_details
+    return patient_details.with_indifferent_access
   end
 
-  def get_population_details_from_measure(measure, results)
-    population_details = {}
-    ####################################################################################
-    # These are snippets from the existing conversion in measure_view.js.coffee
-    ####################################################################################
-    # for pop in @model.get('populations').models
-    #   for patient in @model.get('patients').models
-    #   # Populates the population details
-    #     if (population_details[pop.cid] == undefined)
-    #       population_details[pop.cid] = {title: pop.get("title"), statement_relevance: result.get("statement_relevance")}
-    #       criteria = []
-    #       for popAttrs of pop.attributes
-    #         if (popAttrs != "title" && popAttrs != "sub_id" && popAttrs != "title")
-    #           criteria.push(popAttrs)
-    #       population_details[pop.cid]["criteria"] = criteria
-    ####################################################################################
-
+  def self.get_population_details_from_measure(measure, results)
     population_details = {}
 
-    measure.populations.each do |population|
+    measure.populations.each_with_index do |population, pop_index|
       # Populates the population details
+      next if population_details[pop_index]
 
-      # ignores a population if its details have already been populated
-      # TODO: LDY note - can't use cid here. using 'id'. however, this is a string. it *should* be unique but isn't guaranteed.
-      # other option is an index...
-      next if population_details[population[:id]]
+      # the population_details are independent of patient, so index into the first patient in the results.
+      population_details[pop_index] = {title: population[:title], statement_relevance: results.first[1][population['id']]['extendedData']['statement_relevance']}
 
-      # TODO: LDY note - may need to change "result" -> don't yet know what this looks like
-      population_details[population[:id]] = {title: population[:title], statement_relevance: results[:statement_relevance]}
       criteria = []
-      # TODO: LDY note - comes back with "stratification_index" and "population_index", which I don't believe is intended.
       population.each_key do |key|
         criteria << key if key != "title" && key != "sub_id" && key != "id"
       end
-      population_details[population[:id]][:criteria] = criteria
+      population_details[pop_index][:criteria] = criteria
     end
 
-    population_details
+    population_details.with_indifferent_access
   end
 
   def self.get_statement_details_from_measure(measure)
@@ -100,7 +101,7 @@ module ExcelExportHelper
       statement_details[library_name] = lib_statements
     end
 
-    return statement_details
+    return statement_details.with_indifferent_access
   end
 
   private_class_method def self.parseAnnotationTree(children)
