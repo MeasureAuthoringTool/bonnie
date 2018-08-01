59590f0d94ac58c24f009d5dceb52ab19edc7c6e
Chris Tohline <ctohline@mitre.org>
2018-08-01 10:35:28 -0400

    coments

--- test/functional/patient_builder_test.rb
+++ test/functional/patient_builder_test.rb
@@ -20,17 +20,19 @@ class PatientBuilderFunctionalTest < ActionController::TestCase
     associate_user_with_patients(@user, Record.all)
 
     record = Record.where(last: 'Numerator').first
-    # clear calculated value
+    # clear calculated value from fixture
     record.assessments[0]['values'][0]['scalar'] = '0'
     Measures::PatientBuilder.rebuild_patient(record)
     assert_equal('UnixTime', record.assessments[0]['values'][0]['units'])
+    # this new calculated value should be in seconds
     assert_equal(1341129600, record.assessments[0]['values'][0]['scalar'])
 
     record = Record.where(last: 'NoNumerator').first
-    # clear calculated value
+    # clear calculated value from fixture
     record.assessments[0]['values'][0]['scalar'] = '0'
     Measures::PatientBuilder.rebuild_patient(record)
     assert_equal('UnixTime', record.assessments[0]['values'][0]['units'])
+    # this new calculated value should be in seconds
     assert_equal(1337932800, record.assessments[0]['values'][0]['scalar'])
   end
 
@@ -47,17 +49,19 @@ class PatientBuilderFunctionalTest < ActionController::TestCase
     associate_user_with_patients(@user, Record.all)
 
     record = Record.where(last: 'Numerator').first
-    # clear calculated value
+    # clear calculated value from fixture
     record.vital_signs[0]['components']['values'][0]['result']['scalar'] = '0'
     Measures::PatientBuilder.rebuild_patient(record)
     assert_equal('UnixTime', record.vital_signs[0]['components']['values'][0]['result']['units'])
+    # this new calculated value should be in seconds
     assert_equal(1342512000, record.vital_signs[0]['components']['values'][0]['result']['scalar'])
 
     record = Record.where(last: 'NoNumerator').first
-    # clear calculated value
+    # clear calculated value from fixture
     record.vital_signs[0]['components']['values'][0]['result']['scalar'] = '0'
     Measures::PatientBuilder.rebuild_patient(record)
     assert_equal('UnixTime', record.vital_signs[0]['components']['values'][0]['result']['units'])
+    # this new calculated value should be in seconds
     assert_equal(1310889600, record.vital_signs[0]['components']['values'][0]['result']['scalar'])
   end
 end
