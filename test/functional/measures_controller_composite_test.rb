b04a20a2b56f88ae34910254f01058ab6fbc2b19
Becky Hack <rhack@mitre.org>
2018-10-30 11:19:18 -0400

    New composite measure fixture and test

--- test/functional/measures_controller_composite_test.rb
+++ test/functional/measures_controller_composite_test.rb
@@ -112,4 +112,59 @@ class MeasuresControllerCompositeTest < ActionController::TestCase
     assert_response :redirect
   end
 
+  test "update composite measure with mismatching hqmf_set_id" do
+    # This cassette uses the ENV[VSAC_USERNAME] and ENV[VSAC_PASSWORD] which must be supplied
+    # when the cassette needs to be generated for the first time.
+    measure_file = fixture_file_upload(File.join('test', 'fixtures', 'cql_measure_exports', 'special_measures', 'CMSAWA_v5_6_Artifacts.zip'), 'application/xml')
+
+    # Make sure db is clean
+    assert_equal 0, CqlMeasure.all.count
+
+    # Sanity check
+    measure = CqlMeasure.where({hqmf_set_id: "244B4F52-C9CA-45AA-8BDB-2F005DA05BFC"}).first
+    assert_nil measure
+
+    VCR.use_cassette("valid_vsac_response_composite") do
+      post :create, {
+        vsac_query_type: 'profile',
+        vsac_query_profile: 'Latest eCQM',
+        vsac_query_include_draft: 'false',
+        vsac_query_measure_defined: 'true',
+        vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD'],
+        measure_file: measure_file,
+        measure_type: 'ep',
+        calculation_type: 'patient'
+      }
+    end
+    assert_response :redirect
+
+    measure = CqlMeasure.where({composite: true}).first
+    assert_equal "40280582-6621-2797-0166-4034035B100A", measure['hqmf_id']
+    # This composite measure has 7 components and 1 composite measure
+    assert_equal 8, CqlMeasure.all.count
+
+    #Update previously loaded measure with a measure that has a different hqmf_set_id
+    measure_file = fixture_file_upload(File.join('test', 'fixtures', 'cql_measure_exports', 'special_measures', 'CMSAWA_v5_6_Artifacts_hqmf_set_id_mismatch.zip'), 'application/xml')
+    
+    VCR.use_cassette("valid_vsac_response_composite") do
+      post :create, {
+        vsac_query_type: 'profile',
+        vsac_query_profile: 'Latest eCQM',
+        vsac_query_include_draft: 'false',
+        vsac_query_measure_defined: 'true',
+        vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD'],
+        measure_file: measure_file,
+        measure_type: 'ep',
+        calculation_type: 'patient',
+        hqmf_set_id: "244B4F52-C9CA-45AA-8BDB-2F005DA05BFC"
+      }
+    end
+    assert_equal "Error Updating Measure", flash[:error][:title]
+    assert_equal "The update file does not match the measure.", flash[:error][:summary]
+    assert_equal "You have attempted to update a measure with a file that represents a different measure.  Please update the correct measure or upload the file as a new measure.", flash[:error][:body]
+    assert_response :redirect
+    
+    assert_equal 8, CqlMeasure.all.count
+  end
+
 end
