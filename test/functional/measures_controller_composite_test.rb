27fd8fa44b40d66323780d1e6fb9035cda464169
Lister, Zach (ZLISTER) <zlister@mitre.org>
2018-11-02 14:18:37 -0400

    Added a test to improve code coverage

--- test/functional/measures_controller_composite_test.rb
+++ test/functional/measures_controller_composite_test.rb
@@ -167,4 +167,45 @@ class MeasuresControllerCompositeTest < ActionController::TestCase
     assert_equal 8, CqlMeasure.all.count
   end
 
+
+  test "upload composite cql then try to delete a component" do
+    # This cassette uses the ENV[VSAC_USERNAME] and ENV[VSAC_PASSWORD] which must be supplied
+    # when the cassette needs to be generated for the first time.
+    measure_file = fixture_file_upload(File.join('test', 'fixtures', 'cql_measure_exports', 'special_measures', 'CMSAWA_v5_6_Artifacts.zip'), 'application/xml')
+
+    # Make sure db is clean
+    assert_equal 0, CqlMeasure.all.count
+
+    # Sanity check
+    measure = CqlMeasure.where({hqmf_set_id: "244B4F52-C9CA-45AA-8BDB-2F005DA05BFC"}).first
+    assert_nil measure
+
+    VCR.use_cassette("valid_vsac_response_composite") do
+      post :create, {
+        vsac_query_type: 'profile',
+        vsac_query_profile: 'Latest eCQM',
+        vsac_query_include_draft: 'false',
+        vsac_query_measure_defined: 'true',
+        vsac_username: ENV['VSAC_USERNAME'], vsac_password: ENV['VSAC_PASSWORD'],
+        measure_file: measure_file,
+        measure_type: 'ep',
+        calculation_type: 'patient'
+      }
+    end
+    assert_response :redirect
+
+    measure = CqlMeasure.where({composite: true}).first
+    assert_equal "40280582-6621-2797-0166-4034035B100A", measure['hqmf_id']
+    # This composite measure has 7 components and 1 composite measure
+    assert_equal 8, CqlMeasure.all.count
+
+    # Get a component measure
+    component = CqlMeasure.where({component: true}).first
+    post :destroy, {
+      id: component.id
+    }
+    assert_response :bad_request
+    # Make sure nothing got deleted
+    assert_equal 8, CqlMeasure.all.count
+  end
 end
