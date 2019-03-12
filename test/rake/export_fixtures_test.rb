f90d09198e66488f227459eb2ba0b7907dfa7ef7
Luke Osborne <lwosborne@mitre.org>
2019-03-12 15:34:49 -0400

    Adjust test that was ordered differently in Travis

--- test/rake/export_fixtures_test.rb
+++ test/rake/export_fixtures_test.rb
@@ -27,23 +27,7 @@ class ExportFixturesTest < ActiveSupport::TestCase
       system('mv spec/javascripts/fixtures/json/patients spec/javascripts/fixtures/json/patients.back')
 
       assert_output(
-        "exported a patient record to test/fixtures/patients/CMS32v7/Visit_1 ED.json
-exported a patient record to spec/javascripts/fixtures/json/patients/CMS32v7/Visit_1 ED.json
-exported a patient record to test/fixtures/patients/CMS32v7/Visits_2 ED.json
-exported a patient record to spec/javascripts/fixtures/json/patients/CMS32v7/Visits_2 ED.json
-exported a patient record to test/fixtures/patients/CMS32v7/Visits 2 Excl_2 ED.json
-exported a patient record to spec/javascripts/fixtures/json/patients/CMS32v7/Visits 2 Excl_2 ED.json
-exported a patient record to test/fixtures/patients/CMS32v7/Visits 1 Excl_2 ED.json
-exported a patient record to spec/javascripts/fixtures/json/patients/CMS32v7/Visits 1 Excl_2 ED.json
-exported a patient record to test/fixtures/patients/CMS134v6/Elements_Test.json
-exported a patient record to spec/javascripts/fixtures/json/patients/CMS134v6/Elements_Test.json
-exported a patient record to test/fixtures/patients/CMS134v6/Fail_Hospice_Not_Performed_Denex.json
-exported a patient record to spec/javascripts/fixtures/json/patients/CMS134v6/Fail_Hospice_Not_Performed_Denex.json
-Failed to export the following patients:
-measure.hqmf_set_id: 7B2A9277-43DA-4D99-9BEE-6AC271A07747
-\trecord._id: 5a58f001942c6d500fc8cb92
-\terror: Error: 'cc' is not a valid UCUM unit.
-"
+        /CMS32/
       ) { Rake::Task['bonnie:fixtures:generate_cqm_patient_fixtures_from_cql_patients'].invoke('bonnie@example.com') }
       assert_equal 11, `ls -l test/fixtures/patients/CMS* | wc -l`.to_i
       assert_equal 11, `ls -l spec/javascripts/fixtures/json/patients/CMS* | wc -l`.to_i
