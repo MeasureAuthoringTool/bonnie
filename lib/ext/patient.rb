ed948148cd47ce990b5dca0fe8ebbda17ca764d4
Becky Hack <rhack@mitre.org>
2019-04-09 10:00:21 -0400

    Included query by measure_ids in patient.rb

--- lib/ext/patient.rb
+++ lib/ext/patient.rb
@@ -2,7 +2,7 @@ module CQM
   class Patient
     belongs_to :user
     scope :by_user, ->(user) { where({'user_id'=>user.id}) }
-    scope :by_user_and_hqmf_set_id, ->(user, hqmf_set_id) { where({'user_id'=>user.id }) }
+    scope :by_user_and_hqmf_set_id, ->(user, hqmf_set_id) { where({ 'user_id' => user.id, 'measure_ids' => hqmf_set_id }) }
 
     has_and_belongs_to_many :measures, class_name: 'CQM::Measure'
   end
