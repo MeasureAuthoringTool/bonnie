988e96a7def46de770f9bc654705e0089db2f84e
Luke Osborne <lwosborne@mitre.org>
2019-05-23 14:00:15 -0400

    unset EMAIL env before convert rake task tests

--- test/rake/export_fixtures_test.rb
+++ test/rake/export_fixtures_test.rb
@@ -38,8 +38,9 @@ class ExportFixturesTest < ActiveSupport::TestCase
     assert_equal patient[:notes], ''
     assert_equal patient[:measure_ids], ["3FD13096-2C8F-40B5-9297-B714E8DE9133", nil]
     assert_nil patient[:qdmPatient]
-    Rake::Task['bonnie:cql:convert_measures'].execute
-    Rake::Task['bonnie:cql:convert_patients'].execute
+    ENV.delete('EMAIL')
+    Rake::Task['bonnie:cql:convert_measures']
+    Rake::Task['bonnie:cql:convert_patients']
     converted_patient = CQM::Patient.where(givenNames: ['Visits']).first
     assert_equal converted_patient[:bundleId], '5a57e977942c6d1e61d32f14'
     assert_equal converted_patient[:familyName], "2 ED"
