33c2181b8522ebf09ea887d1d63094300c0dcf8e
Luke Osborne <lwosborne@mitre.org>
2019-06-24 13:52:16 -0400

    report teaspoon all at end in script

--- script/teaspoon_tests.sh
+++ script/teaspoon_tests.sh
@@ -2,11 +2,40 @@
 
 # Script to run all teaspoon tests by directory
 
+# Save the lcov.info files and report all at the end to avoid teaspoon errors on
+# travis.
 bundle exec rake teaspoon DIR='cqm_specs'
+mv ./coverage-frontend/lcov.info ./coverage-frontend/lcov.info.cqm_specs
 bundle exec rake teaspoon DIR='helper_specs'
+mv ./coverage-frontend/lcov.info ./coverage-frontend/lcov.info.helper_specs
 bundle exec rake teaspoon DIR='integration'
+mv ./coverage-frontend/lcov.info ./coverage-frontend/lcov.info.integration
 bundle exec rake teaspoon DIR='models'
+mv ./coverage-frontend/lcov.info ./coverage-frontend/lcov.info.models
 bundle exec rake teaspoon DIR='patient_builder_tests'
+mv ./coverage-frontend/lcov.info ./coverage-frontend/lcov.info.patient_builder_tests
 bundle exec rake teaspoon DIR='production_tests'
+mv ./coverage-frontend/lcov.info ./coverage-frontend/lcov.info.production_tests
 bundle exec rake teaspoon DIR='views'
+mv ./coverage-frontend/lcov.info ./coverage-frontend/lcov.info.views
 bundle exec rake teaspoon DIR='javascripts'
+mv ./coverage-frontend/lcov.info ./coverage-frontend/lcov.info.javascripts
+
+# Send all the reports to github
+# codecov script is expecting the report to be in lcov.info
+mv ./coverage-frontend/lcov.info.cqm_specs ./coverage-frontend/lcov.info
+bash <(curl -s https://codecov.io/bash) -cF cqm_specs
+mv ./coverage-frontend/lcov.info.helper_specs ./coverage-frontend/lcov.info
+bash <(curl -s https://codecov.io/bash) -cF helper_specs
+mv ./coverage-frontend/lcov.info.integration ./coverage-frontend/lcov.info
+bash <(curl -s https://codecov.io/bash) -cF integration
+mv ./coverage-frontend/lcov.info.models ./coverage-frontend/lcov.info
+bash <(curl -s https://codecov.io/bash) -cF models
+mv ./coverage-frontend/lcov.info.patient_builder_tests ./coverage-frontend/lcov.info
+bash <(curl -s https://codecov.io/bash) -cF patient_builder_tests
+mv ./coverage-frontend/lcov.info.production_tests ./coverage-frontend/lcov.info
+bash <(curl -s https://codecov.io/bash) -cF production_tests
+mv ./coverage-frontend/lcov.info.views ./coverage-frontend/lcov.info
+bash <(curl -s https://codecov.io/bash) -cF views
+mv ./coverage-frontend/lcov.info.javascripts ./coverage-frontend/lcov.info
+bash <(curl -s https://codecov.io/bash) -cF javascripts
