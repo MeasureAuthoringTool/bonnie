458e4bb17b3d0e4af75c6c5f8566973664aa952f
Lister, Zach (ZLISTER) <zlister@mitre.org>
2019-06-26 12:56:52 -0400

    no longer need to format some fields

--- lib/patient_export.rb
+++ lib/patient_export.rb
@@ -239,14 +239,7 @@ class PatientExport
   end
 
   def self.add_formatted_patient_field(patient, value)
-    if value == 'ethnicity'
-      return Record::ETHNICITY_NAME_MAP[patient[value]]
-    elsif value == 'race'
-      return Record::RACE_NAME_MAP[patient[value]]
-    elsif value == 'birthdate' || value == 'deathdate'
-      time = Time.at(patient[value]).strftime("%m/%d/%Y") unless patient[value].nil?
-      return time
-    elsif value == 'expired' && patient[value] == nil
+    if value == 'expired' && patient[value] == nil
       return false
     else
       return patient[value]
