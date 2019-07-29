2d60628046cbe37694d7ff4989fae1a29e44d23d
hackrm <42656458+hackrm@users.noreply.github.com>
2019-07-29 15:53:59 -0400

    Population names can be edited and saved properly (#1295)
    * Population names can be edited and saved properly

--- app/controllers/populations_controller.rb
+++ app/controllers/populations_controller.rb
@@ -3,11 +3,10 @@ class PopulationsController < ApplicationController
   def update
     measure = CQM::Measure.by_user(current_user).find(params[:measure_id])
 
-    pop_or_strat = measure.population_sets.select { |ps| ps.population_set_id == params[:id] }.first || measure.all_stratifications.select { |strat| strat.stratification_id == params[:id] }.first
+    pop_or_strat = measure.population_sets.select { |ps| ps.population_set_id == params[:population_set_id] }.first || measure.all_stratifications.select { |strat| strat.stratification_id == params[:population_set_id] }.first
     pop_or_strat.title = params[:title]
     measure.save!
-    # Only return title to client side since that's all we're updating and all we want to overwrite
-    render :json => pop_or_strat.title
+    render :json => pop_or_strat
   end
 
 end
