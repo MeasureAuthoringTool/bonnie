d155d0cf5810f93b650374d9325eb862d94bc906
Matt Mayer <mmayer@mitre.org>
2019-08-14 14:38:25 -0400

    Qrda export 2019 (#1305)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Bonnie::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set log level to info
   config.log_level = :info
