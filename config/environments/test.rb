412e7b137a7c7f6ff516ffd0ce695948bd6afe55
danmee10 <danmee10@gmail.com>
2020-07-28 11:54:40 -0600

    https://guides.rubyonrails.org/5_0_release_notes.html#railties-deprecations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,10 +9,11 @@ Bonnie::Application.configure do
   config.eager_load = false
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_files = true
-  config.static_cache_control = "public, max-age=3600"
-
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=3600"
+  }
   # Log error messages when you accidentally call methods on nil
- 
+
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
