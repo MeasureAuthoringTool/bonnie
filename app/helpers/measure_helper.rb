759ddc859ba387354b375dcfec42bb0d52ad933a
Matthew Gifford <matthew.gifford@semanticbits.com>
2020-08-07 11:02:46 -0700

    Remove "zip" from bad package error message

--- app/helpers/measure_helper.rb
+++ app/helpers/measure_helper.rb
@@ -73,7 +73,7 @@ module MeasureHelper
     def initialize(loading_exception_message)
       front_end_version = {
         title: "Error Uploading Measure",
-        summary: "The uploaded zip file is not a valid Measure Authoring Tool (MAT) export of a FHIR Based Measure.",
+        summary: "The uploaded file is not a valid Measure Authoring Tool (MAT) export of a FHIR Based Measure.",
         body: "#{loading_exception_message.sub(/^#<.*Error: /, '').sub(/>$/, '')} Please re-package and re-export your FHIR based measure from the MAT and try again.".html_safe
       }
       back_end_version = {
