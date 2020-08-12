8198932fb663ace2fb7c110064ef68725c7c7b4f
Joe Kotanchik <joseph.kotanchik@semanticbits.com>
2020-08-12 18:03:29 -0400

    Upping Bonnie version to 5.0.
    - Replacing landing page and login page references to QDM with FHIR.
    - Add configurable properties for supported FHIR Version and current Bonnie version.
    - Replaced bonnie-prior link with link for QDM Bonnie 4.0 and QDM Bonnie 3.2
    - Replaced reference to MITRE with SemanticBits.

--- lib/version.rb
+++ lib/version.rb
@@ -1,10 +1,13 @@
 module Bonnie
   class Version
 
-    def self.current()
+    def self.current
       return APP_CONFIG["version"]
     end
 
+    def self.fhir_ver
+      return APP_CONFIG['support_fhir_version']
+    end
 
   end
 end 
