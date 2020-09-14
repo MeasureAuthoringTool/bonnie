94bcc9d8778a5af7edbf27a2f036f8a9460990f3
Joe Kotanchik <56264529+jkotanchik-SB@users.noreply.github.com>
2020-09-14 13:51:41 -0400

    [MAT-1759] Replacing VSAC username/password with API Key (#1524)
    * Replacing the VSAC username/password with user API key in VSAC API calls.
    
    * Replacing references to VSAC username and password with API Key from error messages. Removing HQMF prefixes.
    
    * Replacing vsac username/password with api key in tests.
    
    * Replacing vsac username/password with api key in rake task.
    
    * Updating cassettes to use vsac api key instead of username/password.
    
    * Upping cqm-parsers ref in gemfile lock.

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -191,12 +191,12 @@ class MeasuresController < ApplicationController
       # prepeared a measure upload assuming ticket_granting_ticket in the session in the first tab
 
       # First make sure we have credentials to attempt getting a ticket with. Throw an error if there are no credentials.
-      if params[:vsac_username].nil? || params[:vsac_password].nil?
+      if params[:vsac_api_key].nil?
         raise Util::VSAC::VSACNoCredentialsError.new
       end
 
       # Retrieve a new ticket granting ticket by creating the api class.
-      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], username: params[:vsac_username], password: params[:vsac_password])
+      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], api_key: params[:vsac_api_key])
       ticket_granting_ticket = api.ticket_granting_ticket
 
       # Create a new ticket granting ticket session variable
--- test/vcr_setup.rb
+++ test/vcr_setup.rb
@@ -7,15 +7,13 @@ VCR.configure do |c|
   c.hook_into :webmock
   
   # To avoid storing plain text VSAC credentials or requiring the VSAC credentials
-  # be provided at every run of the rake tests, provide the VSAC_USERNAME and VSAC_PASSWORD
+  # be provided at every run of the rake tests, provide the VSAC_API_KEY
   # whenever you need to record a cassette that requires valid credentials
-  ENV['VSAC_USERNAME'] = "vcrtest" unless ENV['VSAC_USERNAME']
-  ENV['VSAC_PASSWORD'] = "vcrpass" unless ENV['VSAC_PASSWORD']
+  ENV['VSAC_API_KEY'] = "vcrpass" unless ENV['VSAC_API_KEY']
 
   # Ensure plain text passwords do not show up during logging
-  c.filter_sensitive_data('<VSAC_USERNAME>') {ENV['VSAC_USERNAME']}
-  c.filter_sensitive_data('<VSAC_PASSWORD>') {URI.escape(ENV['VSAC_PASSWORD'])}
-  c.default_cassette_options = {record: :once }
+  c.filter_sensitive_data('<VSAC_API_KEY>') {URI.escape(ENV['VSAC_API_KEY'])}
+  c.default_cassette_options = { record: :once }
 
   # Add a custom matcher for use with the bulk request by typheous, so we can ignore service ticket
   c.register_request_matcher :uri_no_st do |req1, req2|
