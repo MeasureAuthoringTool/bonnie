fc168965ae45d452ebb97a8512275e1cb8227ff7
Matthew Gifford <matthew.gifford@semanticbits.com>
2020-09-25 11:00:41 -0700

    Whitelist criteria-icons

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,1 +1,1 @@
-NonStupidDigestAssets.whitelist = [/fontawesome/, /FontAwesome/, 'loading.gif', 'Rabbit-96-blue.png', 'criteria-icons']
+NonStupidDigestAssets.whitelist = [/fontawesome/, /FontAwesome/, 'loading.gif', 'Rabbit-96-blue.png', /criteria-icons/]
