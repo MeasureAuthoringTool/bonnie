f4d3c544c01fb6ae8e34ae670e2a77ada9650131
danmee10 <danmee10@gmail.com>
2020-10-08 07:17:50 -0600

    added log filters

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,8 @@ module Bonnie
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.filter_parameters += [:vsac_api_key]
+    config.filter_parameters += [:api_key]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
