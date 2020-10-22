7579878b52b402ddcb1ea8265d5eba7e92f2cd2a
Brendan Donohue <brendan.donohue@semanticbits.com>
2020-10-22 14:09:51 -0400

    email

---
+++ config/deploy/development.rb
@@ -0,0 +1,25 @@
+# Capistrano is run on the deploy-to server via a Bamboo hook, so just deploy to the current host and user
+# account into a directory relative to our current directory
+
+# Deploy with production environment
+set :stage, :development
+
+# Tell RVM to use the current ruby when running capistrano
+set :rvm1_ruby_version, '2.4.5'
+
+# Set the branch to the currently checked out branch
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Keep a few older releases
+set :keep_releases, 3
+
+# Hostname and secret token get specified in config files local to the server
+set :linked_files, (fetch(:linked_files) || []).concat(%w{config/server.yml config/newrelic.yml config/secrets.yml config/email.yml})
+
+# Repository gets cloned to /apps/dev/tacoma/ruby/repository/bonnie, bonnie gets deployed to /apps/dev/tacoma/ruby/bonnie
+set :deploy_to, File.join(File.dirname(File.dirname(Dir.pwd)), 'bonnie')
+
+# Login using the username running the cap process (not necessarily the login user returned by Etc.getlogin!)
+role :app, "#{Etc.getpwuid(Process.uid).name}@localhost"
+role :web, "#{Etc.getpwuid(Process.uid).name}@localhost"
+role :db,  "#{Etc.getpwuid(Process.uid).name}@localhost"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,27 @@ Bonnie::Application.configure do
 
   # Load npm modules into the asset path
   config.assets.paths << Rails.root.join('node_modules')
+
+  # Configure to send email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              APP_CONFIG['smtp_hostname'],
+    port:                 APP_CONFIG['smtp_port'],
+    user_name:            APP_CONFIG['smtp_username'],
+    password:             APP_CONFIG['smtp_password'],
+    authentication:       'plain',
+    enable_starttls_auto: APP_CONFIG['smtp_tls'],
+    tls:                  APP_CONFIG['smtp_tls']
+  }
+
+  # Send notification when application exceptions happen
+  config.middleware.use ExceptionNotification::Rack, email: {
+    email_prefix: "[Bonnie] ",
+    sender_address: %{"Bonnie (#{APP_CONFIG['hostname']})" <bonnie@#{APP_CONFIG['hostname']}>},
+    exception_recipients: APP_CONFIG['bonnie_error_email'],
+    sections: %w{request session user_info environment backtrace}
+  }, error_grouping: true
+
 end
