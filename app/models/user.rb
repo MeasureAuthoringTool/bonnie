901c4fbc4292dd7e232464718a29201f35f0df0f
Serhii Ilin <serhii.ilin@semanticbits.com>
2021-03-16 13:43:54 -0400

    MAT-2764 Create Script to Update Bonnie Database to Use New Group Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -161,7 +161,7 @@ class User
   # Measure and patient counts can be pre-populated or just retrieved
   attr_writer :measure_count
   def measure_count
-    @measure_count || cqm_measures.count
+    @measure_count || @current_group.cqm_measures.count
   end
 
   attr_writer :patient_count
