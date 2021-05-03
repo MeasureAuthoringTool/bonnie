08ca9071e7d5f99ec335e8dbbe5e79cfd9ef42ee
Ashok Dongare <ashok.dongare@semanticbits.com>
2021-05-03 12:21:54 -0400

    MAT-2866 codacy checks

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -2,13 +2,12 @@ class Admin::GroupsController < ApplicationController
   protect_from_forgery
   before_action :authenticate_user!
   before_action :require_admin!
-
   respond_to :json
 
   def index
     groups = Group.where(is_personal: false).order(:name.asc).to_a
     # pipeline stages for aggregation
-    stages =  [
+    stages = [
       {
         "$group" => {
           "_id" => "$group_id",
@@ -16,15 +15,12 @@ class Admin::GroupsController < ApplicationController
         }
       }
     ]
-
     measures = CQM::Measure.collection.aggregate(stages).as_json
     patients = CQM::Patient.collection.aggregate(stages).as_json
-
     groups.each do |group|
       group[:measure_count] = get_count_for_group(measures, group)
       group[:patient_count] = get_count_for_group(patients, group)
     end
-
     respond_with groups do |format|
       format.json { render json: groups.as_json }
     end
