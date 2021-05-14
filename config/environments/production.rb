1ed761e42a376902469ea38522995ad039bd956a
Brendan Donohue <brendan.donohue@semanticbits.com>
2021-05-14 15:16:36 -0400

    adding ssl redir back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,8 +31,7 @@ Bonnie::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # will be handled at F5 
-  #config.force_ssl = true
+  config.force_ssl = true
 
   # Set log level to info
   config.log_level = :info
