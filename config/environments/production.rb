a7f62fc6ca2e5f1e3089ace5b83d469f027e632b
Brendan Donohue <brendan.donohue@semanticbits.com>
2021-06-07 14:39:38 -0400

    configure prod to send logs to stdout so it can be sent to cw and splunk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Bonnie::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
