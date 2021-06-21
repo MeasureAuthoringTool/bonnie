aebf9609e1522db99c2ca74ab670200da2ade354
Serhii Ilin <serhii.ilin@semanticbits.com>
2021-06-21 13:35:37 -0400

    MAT-3049 Spike: investigate case-sensitive login w/ auto-linking

--- test/functional/saml_failure_handler_test.rb
+++ test/functional/saml_failure_handler_test.rb
@@ -32,8 +32,45 @@ class SamlFailureHandlerTest < ActionController::TestCase
     expected_msg = 'Your HARP Account with bonnie_harp_id and Bonnie account with bonnie@example.com have been automatically linked. '\
     'If you need further assistance please reach out to the Bonnie Help Desk.'
     assert_equal(expected_msg, flash[:msg][:body])
-    assert_equal(true, @user.approved)
+    assert_equal(true, @user.is_approved?)
     assert_equal('bonnie_harp_id', @user.harp_id)
+  end
+
+  test "link user by email case insensitive" do
+    @user.approved = true
+    @user.save
+    flash = Hash.new
+    handler = SamlFailureHandler.new
+    redirected = handler.link_user('Bonnie@Example.Com', 'bonnie_harp_id', flash)
+    @user = User.by_email('Bonnie@Example.Com').first
+    assert_match('/users/saml/sign_in', redirected)
+    assert_equal('HARP Account Linked', flash[:msg][:title])
+    assert_equal('HARP Account Linked', flash[:msg][:summary])
+    expected_msg = 'Your HARP Account with bonnie_harp_id and Bonnie account with Bonnie@Example.Com have been automatically linked. '\
+    'If you need further assistance please reach out to the Bonnie Help Desk.'
+    assert_equal(expected_msg, flash[:msg][:body])
+    assert_equal(true, @user.is_approved?)
+    assert_equal('bonnie_harp_id', @user.harp_id)
+    assert_equal('Bonnie@Example.Com', @user.email)
+  end
+
+  test "link user by harp id case insensitive" do
+    @user.approved = true
+    @user.harp_id = 'bonnie_harp_Id'
+    @user.save
+    flash = Hash.new
+    handler = SamlFailureHandler.new
+    redirected = handler.link_user('Bonnie@Example.Com', 'Bonnie_Harp_Id', flash)
+    @user = User.by_email('Bonnie@Example.Com').first
+    assert_match('/users/saml/sign_in', redirected)
+    assert_equal('HARP Account Linked', flash[:msg][:title])
+    assert_equal('HARP Account Linked', flash[:msg][:summary])
+    expected_msg = 'Your HARP Account with Bonnie_Harp_Id and Bonnie account with Bonnie@Example.Com have been automatically linked. '\
+    'If you need further assistance please reach out to the Bonnie Help Desk.'
+    assert_equal(expected_msg, flash[:msg][:body])
+    assert_equal(true, @user.is_approved?)
+    assert_equal('Bonnie_Harp_Id', @user.harp_id)
+    assert_equal('Bonnie@Example.Com', @user.email)
   end
 
   test "link user not approved" do
@@ -46,7 +83,7 @@ class SamlFailureHandlerTest < ActionController::TestCase
     @user = User.by_email('bonnie@example.com').first
     assert_match('/user/registered_not_active', redirected)
     assert_empty(flash)
-    assert_equal(false, @user.approved)
+    assert_equal(false, @user.is_approved?)
     assert_nil(@user.harp_id)
   end
 
