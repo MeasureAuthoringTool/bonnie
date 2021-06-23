25727a2eb1fe0cb7db6c715a0ee01a33d030cacb
Michael C Greene <greenemcg@gmail.com>
2021-06-23 14:46:07 -0600

    removed spatial index and checked for dup groups in application

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group
   include Mongoid::Attributes::Dynamic
 
   index({ name: 1 },
-        { unique: true, name: "group_name_index", collation: { locale: "en", strength: 2 } })
+        { unique: true, name: "group_name_index" })
 
   has_many :cqm_measures, class_name: 'CQM::Measure'
   has_many :patients, class_name: 'CQM::Patient'
