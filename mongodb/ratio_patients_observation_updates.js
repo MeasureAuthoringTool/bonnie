f6514987ec38c76474c0b0f563b9e7f9668e85c6
Ashok Dongare <ashok.dongare@semanticbits.com>
2023-07-27 11:09:30 -0400

    MAT-5993: Ratio observation data migration and Excel export issues (#2161)
    * MAT-5993 ratio observation migration script
    
    * MAT-5860 added couple more test cases for ratio measures actual vs expected values
    
    * MAT-5860 removed unwanted code
    
    * MAT-5993 Ratio patient excel Export issues
    
    * MAT-5993 ratio patient delete OBSERV
    
    * more test cases
    
    * MAT-5859 remove unit switch

--- mongodb/ratio_patients_observation_updates.js
+++ mongodb/ratio_patients_observation_updates.js
@@ -1,26 +1,27 @@
+// this script removes OBSERV array from ratio measure patients
 print("Observation data removal started...");
 const patientsUpdated = {}
 db.getCollection('cqm_patients').find().forEach((patient) => {
-  print(`-------Updating patient : ${patient._id} ------------` );
+  print(`Patient id : ${patient._id}` );
   const expectedValues = patient.expectedValues;
-  // empty patient, not possible but if in case
   if (!expectedValues) {
+    print('No expected values present' );
     return;
   }
   expectedValues.forEach((expectedValue) => {
     if (expectedValue.hasOwnProperty('OBSERV') && !expectedValue.hasOwnProperty('MSRPOPL')) {
-      delete expectedValue.OBSERV
+      print(`Deleting OBSERV for patient : ${patient._id}` );
+      db.getCollection('cqm_patients').updateOne(
+        { _id: patient._id},
+        {
+          $unset: {'expectedValues.$[].OBSERV': ""}
+        }
+      );
       patientsUpdated[`${patient._id}`] =`${patient.familyName} ${patient.givenNames[0]}`
+    } else {
+      print(`Does not have OBSERV or not a ratio patient: ${patient._id}`);
     }
   })
-  // update patient
-  if (patientsUpdated[`${patient._id}`]) {
-    db.getCollection('cqm_patients').update(
-      { _id: patient._id},
-      patient,
-      { multi: false }
-    );
-  }
 });
 print(JSON.stringify(patientsUpdated));
 print("Number of patients updated: ", Object.keys(patientsUpdated).length);
